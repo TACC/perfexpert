5b85a707f231c189697d71704efd99657ee50b6a
Leonardo Fialho <leonardofialho@gmail.com>
2013-02-11 13:58:14 -0600

    Code refactoring.

--- tools/perfexpert/perfexpert/parsing/AParser.java
+++ tools/perfexpert/perfexpert/parsing/AParser.java
@@ -21,19 +21,16 @@
 
 package edu.utexas.tacc.perfexpert.parsing;
 
-public abstract class AParser
-{
-	protected double threshold = 0.1;
-	protected String sourceURI = null;
+public abstract class AParser {
+    protected double threshold = 0.1;
+    protected String sourceURI = null;
 
-	public AParser()
-	{
-		;
-	}
+    public AParser() {
+        ;
+    }
 
-	public AParser(double threshold, String sourceURI)
-	{
-		this.threshold = threshold;
-		this.sourceURI = sourceURI;
-	}
+    public AParser(double threshold, String sourceURI) {
+        this.threshold = threshold;
+        this.sourceURI = sourceURI;
+    }
 }
--- tools/perfexpert/perfexpert/parsing/hpctoolkit/HPCToolkitParser.java
+++ tools/perfexpert/perfexpert/parsing/hpctoolkit/HPCToolkitParser.java
@@ -29,197 +29,179 @@ import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.util.List;
 import java.util.Properties;
-
 import javax.xml.parsers.SAXParserFactory;
-
 import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
 
 import edu.utexas.tacc.perfexpert.parsing.AParser;
 import edu.utexas.tacc.perfexpert.parsing.profiles.hpctoolkit.HPCToolkitProfile;
 
-public class HPCToolkitParser extends AParser
-{
-	private Logger log = Logger.getLogger( HPCToolkitParser.class );
-	
-	RandomAccessFile file;
-	Properties LCPIConfig;
-	List<HPCToolkitProfile> profiles = null;
-	
-	private String HPCDataLocation;
+public class HPCToolkitParser extends AParser {
+    private Logger log = Logger.getLogger( HPCToolkitParser.class );
+    
+    RandomAccessFile file;
+    Properties LCPIConfig;
+    List<HPCToolkitProfile> profiles = null;
+    
+    private String HPCDataLocation;
 
-	public HPCToolkitParser(String HPCDataLocation, double threshold, String sourceURI, Properties LCPIConfig)
-	{
-		super(threshold, sourceURI);
-		this.LCPIConfig = LCPIConfig;
-		this.HPCDataLocation = HPCDataLocation;
-	}
-	
-	public List<HPCToolkitProfile> getAllProfiles()
-	{
-		return profiles;
-	}
+    public HPCToolkitParser(String HPCDataLocation, double threshold,
+                            String sourceURI, Properties LCPIConfig) {
+        super(threshold, sourceURI);
+        this.LCPIConfig = LCPIConfig;
+        this.HPCDataLocation = HPCDataLocation;
+    }
+    
+    public List<HPCToolkitProfile> getAllProfiles() {
+        return profiles;
+    }
 
-	public List<HPCToolkitProfile> parse(boolean aggregateOnly, String regex)
-	{
-		if (profiles != null && profiles.size() != 0)
-		{
-			// We have already parsed the input once, don't parse again;
-			return profiles;
-		}
-		
-		log.debug("Beginning to parse \"" + sourceURI + "\"");
-		
-		// Figure out if we are going to parse from raw data or file or something else
-		int fileIndex = sourceURI.indexOf("://");
-		if (sourceURI.substring(0, fileIndex).equals("file"))
-		{
-			String filename = sourceURI.substring("file://".length());
-			String convertedFilename = filename + ".converted";
-			
-			// Check if File exists
-			if (!new File (filename).exists())
-			{
-				log.error("Input XML file does not exist: " + filename);
-				return profiles;
-			}
+    public List<HPCToolkitProfile> parse(boolean aggregateOnly, String regex) {
+        int fileIndex = sourceURI.indexOf("://");
 
-			// Convert to simpler XML format to be used with parsing
-			Process p;
-			StringBuilder err = new StringBuilder();
-			try
-			{
-				log.debug("Converting input file \"" + filename + "\" to flat-profile XML: \"" + convertedFilename + "\"");
-				if (!new File(HPCDataLocation + "/hpcdata.sh").exists())
-				{
-					log.error("Could not locate HPCToolkit conversion program at: " + HPCDataLocation + "/hpcdata.sh");
-					return null;
-				}
+        if ((null != profiles) && (0 != profiles.size())) {
+            // We have already parsed the input once, don't parse again;
+            return profiles;
+        }
+        
+        log.debug("Beginning to parse \"" + sourceURI + "\"");
+        
+        // Figure out if we are going to parse from raw data or file or something else
+        if (sourceURI.substring(0, fileIndex).equals("file")) {
+            String filename = sourceURI.substring("file://".length());
+            String convertedFilename = filename + ".converted";
+            Process p;
+            StringBuilder err = new StringBuilder();
+            
+            // Check if File exists
+            if (!new File (filename).exists()) {
+                log.error("Input XML file does not exist: " + filename);
+                return profiles;
+            }
 
-				p = new ProcessBuilder("/bin/sh", HPCDataLocation + "/hpcdata.sh", "-o=" + convertedFilename, filename).start();
-				
-				// Check if there were errors
-				byte [] byteArray = new byte [1024];
-				while (p.getErrorStream().read(byteArray) > 0)
-					err.append(new String(byteArray));
-				
-				if (new String(err).contains("Exception"))
-				{
-					log.error("Error converting input file \"" + filename + "\":\n" + err);
-					return profiles;
-				}
-				else
-				{
-					log.debug("Conversion complete");
-					System.out.println("Input file: \"" + filename + "\"");
-				}
-			}
-			catch (IOException e)
-			{
-				log.error("Conversion failed: " + e);
-				e.printStackTrace();
-				return profiles;
-			}
-			
-			// Get the big wheels turning...
-			// Parse the XML now and load profiles
-			process(convertedFilename, aggregateOnly, regex);
-			
-			// Sort profiles in descending order by importance, ignore (0) because it contains the root (aggregate) profile
-			// Collections.sort(profiles);
-			for (int i=1; i<profiles.size()-1; i++)
-			{
-				for (int j=i+1; j<profiles.size(); j++)
-				{
-					if (profiles.get(i).getImportance() < profiles.get(j).getImportance())
-					{
-						// Swap
-						HPCToolkitProfile temp = profiles.get(i);
-						profiles.set(i, profiles.get(j));
-						profiles.set(j, temp);
-					}
-				}
-			}
-		}
-		else
-			log.error("Unsupported URI type for input file in \"" + sourceURI + "\"");
+            // Convert to simpler XML format to be used with parsing
+            try {
+                log.debug("Converting input file \"" + filename +
+                          "\" to flat-profile XML: \"" + convertedFilename +
+                          "\"");
+                if (!new File(HPCDataLocation + "/hpcdata").exists()) {
+                    log.error("Could not locate HPCToolkit conversion program at: " +
+                              HPCDataLocation + "/hpcdata");
+                    return null;
+                }
 
-		return profiles;
-	}
+                p = new ProcessBuilder("/bin/sh", HPCDataLocation + "/hpcdata",
+                                       "-o=" + convertedFilename,
+                                       filename).start();
+                
+                // Check if there were errors
+                byte [] byteArray = new byte [1024];
+                while (p.getErrorStream().read(byteArray) > 0)
+                    err.append(new String(byteArray));
+                
+                if (new String(err).contains("Exception")) {
+                    log.error("Error converting input file \"" + filename +
+                              "\":\n" + err);
+                    return profiles;
+                } else {
+                    log.debug("Conversion complete");
+                    System.out.println("Input file: \"" + filename + "\"");
+                }
+            }
+            catch (IOException e) {
+                log.error("Conversion failed: " + e);
+                e.printStackTrace();
+                return profiles;
+            }
+            
+            // Get the big wheels turning...
+            // Parse the XML now and load profiles
+            process(convertedFilename, aggregateOnly, regex);
+            
+            // Sort profiles in descending order by importance, ignore (0) because it contains the root (aggregate) profile
+            // Collections.sort(profiles);
+            for (int i = 1; i < profiles.size()-1; i++) {
+                for (int j = i + 1; j < profiles.size(); j++) {
+                    if (profiles.get(i).getImportance() < profiles.get(j).getImportance()) {
+                        // Swap
+                        HPCToolkitProfile temp = profiles.get(i);
+                        profiles.set(i, profiles.get(j));
+                        profiles.set(j, temp);
+                    }
+                }
+            }
+        } else {
+            log.error("Unsupported URI type for input file in \"" + sourceURI +
+                      "\"");
+        }
+        return profiles;
+    }
 
-	void doXMLParsing(String filename, boolean aggregateOnly, String regex)
-	{
-		HPCToolkitXMLParser xmlParser = new HPCToolkitXMLParser();
-		xmlParser.setThreshold(threshold);
-		xmlParser.setLCPIConfig(LCPIConfig);
-		xmlParser.setIfAggregateOnly(aggregateOnly);
-		xmlParser.setThreadRegex(regex);
+    void doXMLParsing(String filename, boolean aggregateOnly, String regex) {
+        HPCToolkitXMLParser xmlParser = new HPCToolkitXMLParser();
+        xmlParser.setThreshold(threshold);
+        xmlParser.setLCPIConfig(LCPIConfig);
+        xmlParser.setIfAggregateOnly(aggregateOnly);
+        xmlParser.setThreadRegex(regex);
 
-		SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+        SAXParserFactory parserFactory = SAXParserFactory.newInstance();
 
-		try
-		{
-			javax.xml.parsers.SAXParser parser = parserFactory.newSAXParser();
-			parser.parse(filename, xmlParser);
-		}
-		catch (XMLParsingDoneException e)
-		{
-			// This just indicates that we finished (rather terminated) XML processing.
-			// This never implies an unclean termination, so we don't throw an error.
+        try {
+            javax.xml.parsers.SAXParser parser = parserFactory.newSAXParser();
+            parser.parse(filename, xmlParser);
+        }
+        catch (XMLParsingDoneException e) {
+            // This just indicates that we finished (rather terminated) XML processing.
+            // This never implies an unclean termination, so we don't throw an error.
+            ;
+        }
+        catch (SAXException e) {
+            log.error("Error parsing converted file: " + filename + "\n" +
+                      e.getMessage());
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            log.error("Error parsing converted file: " + filename + "\n" +
+                      e.getMessage());
+            e.printStackTrace();
+        }
 
-			;
-		}
-		catch (SAXException e)
-		{
-			log.error("Error parsing converted file: " + filename + "\n" + e.getMessage());
-			e.printStackTrace();
-		}
-		catch (Exception e)
-		{
-			log.error("Error parsing converted file: " + filename + "\n" + e.getMessage());
-			e.printStackTrace();
-		}
+        this.profiles = xmlParser.getProfileList(); 
+        log.debug("Recorded " + profiles.size() + " profiles");
+    }
+    
+    void process(String filename, boolean aggregateOnly, String regex) {
+        // Fire up the XML parser and get the list of collected profiles
+        // Profiles are saved from within the method.. Bad design? Likely!
+        doXMLParsing(filename, aggregateOnly, regex);
+        
+        if (0 == profiles.size()) {
+            log.error("Collected zero profiles from HPCToolkit. Is the input file valid?");
+            return;
+        }
 
-		this.profiles = xmlParser.getProfileList(); 
-		log.debug("Recorded " + profiles.size() + " profiles");
-	}
-	
-	void process(String filename, boolean aggregateOnly, String regex)
-	{
-		// Fire up the XML parser and get the list of collected profiles
-		// Profiles are saved from within the method.. Bad design? Likely!
-		doXMLParsing(filename, aggregateOnly, regex);
-		
-		if (profiles.size() == 0)
-		{
-			log.error("Collected zero profiles from HPCToolkit. Is the input file valid?");
-			return;
-		}
+        // Sanity check
+        int indexOfInstructions = profiles.get(0).getConstants().getIndexOfInstructions();
+        if (0 > indexOfInstructions) {
+            log.error("Could not find PAPI_TOT_INS in the list of performance counters that HPCToolkit recorded. Is the input file valid?");
+            return;
+        }
+        
+        // Processed input file without errors, so remove the converted file from disk
+        new File(filename).delete();
+    }
 
-		// Sanity check
-		int indexOfInstructions = profiles.get(0).getConstants().getIndexOfInstructions();
-		if (indexOfInstructions < 0)
-		{
-			log.error("Could not find PAPI_TOT_INS in the list of performance counters that HPCToolkit recorded. Is the input file valid?");
-			return;
-		}
-		
-		// Processed input file without errors, so remove the converted file from disk
-		new File(filename).delete();
-	}
-
-	// Used while saving the output of the conversion
-	void writeStreamToFile(InputStream is, String filename) throws IOException
-	{
-		File outputFile = new File(filename);
-		OutputStream out = new FileOutputStream(outputFile);
-		
-		int len;
-		byte [] buff = new byte[1024];
-		while ((len = is.read(buff)) > 0)
-			out.write(buff, 0, len);
-		
-		out.close();
-		is.close();
-	}
+    // Used while saving the output of the conversion
+    void writeStreamToFile(InputStream is, String filename) throws IOException {
+        File outputFile = new File(filename);
+        OutputStream out = new FileOutputStream(outputFile);
+        
+        int len;
+        byte [] buff = new byte[1024];
+        while (0 < (len = is.read(buff))) {
+            out.write(buff, 0, len);
+        }
+        out.close();
+        is.close();
+    }
 }
