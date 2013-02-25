596af1264e4ffe67d7721a827a30a526b1fdd5bc
Leonardo Fialho <leonardofialho@gmail.com>
2013-02-25 17:53:38 -0600

    Adding a OPTTRAN_UNDEFINED constant.

--- tools/common/opttran_constants.h
+++ tools/common/opttran_constants.h
@@ -38,12 +38,14 @@ extern "C" {
  * it is possible to combine more than one error code in the return value.
  */
 
+/** OPTTRAN_UNDEFINED should be used when it is not an error */
+#define OPTTRAN_UNDEFINED -2
 /** OPTTRAN_FAILURE should be used when an error is -1 */
-#define OPTTRAN_FAILURE  -1
+#define OPTTRAN_FAILURE   -1
 /** OPTTRAN_SUCCESS should be used in case of success */
-#define OPTTRAN_SUCCESS  0
+#define OPTTRAN_SUCCESS    0
 /** OPTTRAN_ERROR should be used in case of general error */
-#define OPTTRAN_ERROR    1
+#define OPTTRAN_ERROR      1
 
 #define OPTTRAN_TRUE   1 /**< used to return boolean values */
 #define OPTTRAN_FALSE  0 /**< used to return boolean values */
