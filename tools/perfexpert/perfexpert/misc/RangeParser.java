90c9c92f5240382e222ad70cd3d96c36d701f477
Leonardo Fialho <leonardofialho@gmail.com>
2013-05-07 14:04:51 -0500

    Removing the name OptTran from the HEADER of files.

--- tools/perfexpert/perfexpert/misc/RangeParser.java
+++ tools/perfexpert/perfexpert/misc/RangeParser.java
@@ -31,6 +31,21 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This file is part of PerfExpert.
+ *
+ * PerfExpert is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * PerfExpert is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PerfExpert. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package edu.utexas.tacc.perfexpert.misc;
