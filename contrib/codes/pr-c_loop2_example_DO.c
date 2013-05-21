85121b870330abb8dcae27c0aec3b4bef7bdf3ef
Leonardo Fialho <leonardofialho@gmail.com>
2013-05-21 12:55:34 -0500

    Tentative fix to the workflow script. Removing example files.

--- contrib/codes/pr-c_loop2_example_DO.c
+++
@@ -1,7 +0,0 @@
-do {
-    do {
-        c[i][j] += (a[i][j] * b[i][j]);
-        j++;
-    } while (j < M);
-    i++;
-} while (i < N);
--- contrib/codes/pr-c_loop2_example_FOR.c
+++
@@ -1,5 +0,0 @@
-for (i = A; i < N; i++) {
-    for (j = B; j < M; k++) {
-        c[i][j] += (a[i][j] * b[i][j]);
-    }
-}
