6e7c4d7484b476928c4d653af4df3c836c2c9eb3
Ashay Rane <ashay.rane@utexas.edu>
2013-06-07 01:45:47 -0500

    Added 'extern "C"' declarations to headers

--- tools/minst/libmrt/mrt.h
+++ tools/minst/libmrt/mrt.h
@@ -20,18 +20,57 @@ static volatile sig_atomic_t sleeping=0, access_count=0;
 static int fd=-1, sleep_sec=0, new_sleep_sec=1, *intel_apic_mapping=NULL;
 static node_t terminal_node;
 
+#if	defined(__cplusplus)
+extern "C" {
+#endif
 void indigo__init_();
-static void indigo__exit();
+#if	defined(__cplusplus)
+}
+#endif
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+static void indigo__exit();
+#if defined (__cplusplus)
+}
+#endif
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
 void indigo__write_idx_c(const char* var_name, const int length);
+#if defined (__cplusplus)
+}
+#endif
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
 void indigo__write_idx_f_(const char* var_name, const int* length);
+#if defined (__cplusplus)
+}
+#endif
 
 static inline int getCoreID();
 
 static inline void signalHandler(int sig);
 static inline void fill_struct(int read_write, int line_number, size_t p, int var_idx);
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
 void indigo__record_c(int read_write, int line_number, void* addr, int var_idx);
+#if defined (__cplusplus)
+}
+#endif
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
 void indigo__record_f_(int* read_write, int* line_number, void* addr, int* var_idx);
+#if defined (__cplusplus)
+}
+#endif
 
 #endif	/* LIBMRT_H_ */
