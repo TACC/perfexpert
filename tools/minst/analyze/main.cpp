b8ec6878701e51d850176da88bb43f5155e10777
Ashay Rane <ashay.rane@utexas.edu>
2013-06-08 19:54:06 -0500

    Minor: Removing extra spaces in bot output.

--- tools/minst/analyze/main.cpp
+++ tools/minst/analyze/main.cpp
@@ -904,20 +904,20 @@ int main(int argc, char* argv[])
 			{
 				for (int i=0; i<3 && i<stride_list.size(); i++)
 				{
-					printf ("%s.stride_%d.value = %ld\n", var_name.c_str(), i, stride_list[i].first);
-					printf ("%s.stride_%d.count = %ld\n", var_name.c_str(), i, stride_list[i].second);
+					printf ("%s.stride_%d.value=%ld\n", var_name.c_str(), i, stride_list[i].first);
+					printf ("%s.stride_%d.count=%ld\n", var_name.c_str(), i, stride_list[i].second);
 				}
 
-				// printf ("%s.avg_dist = %.2f\n", var_name.c_str(), ptr->tot_dist /((double) ptr->tot_count));
-				printf ("%s.count = %ld\n", var_name.c_str(), ptr->tot_count);
-				printf ("%s.avg_cpa = %.2f\n", var_name.c_str(), avg_cpa);
-				printf ("%s.l1_conflict_ratio = %.0f%%\n", var_name.c_str(), l1_conflict_ratio);
-				printf ("%s.l2_conflict_ratio = %.0f%%\n", var_name.c_str(), l2_conflict_ratio);
-				// printf ("%s.l3_conflict_ratio = %.0f%%\n", var_name.c_str(), 100.0 * (1-ptr->l3_hit / ((double) (ptr->l3_hit + ptr->l3_miss))));
-				printf ("%s.numa_conflict_ratio = %.0f\n", var_name.c_str(), numa_conflict_ratio);
-				printf ("%s.l1_reuse = %.0f%%\n", var_name.c_str(), l1_reuse);
-				printf ("%s.l2_reuse = %.0f%%\n", var_name.c_str(), l2_reuse);
-				printf ("%s.l3_reuse = %.0f%%\n", var_name.c_str(), l3_reuse);
+				// printf ("%s.avg_dist=%.2f\n", var_name.c_str(), ptr->tot_dist /((double) ptr->tot_count));
+				printf ("%s.count=%ld\n", var_name.c_str(), ptr->tot_count);
+				printf ("%s.avg_cpa=%.2f\n", var_name.c_str(), avg_cpa);
+				printf ("%s.l1_conflict_ratio=%.0f%%\n", var_name.c_str(), l1_conflict_ratio);
+				printf ("%s.l2_conflict_ratio=%.0f%%\n", var_name.c_str(), l2_conflict_ratio);
+				// printf ("%s.l3_conflict_ratio=%.0f%%\n", var_name.c_str(), 100.0 * (1-ptr->l3_hit / ((double) (ptr->l3_hit + ptr->l3_miss))));
+				printf ("%s.numa_conflict_ratio=%.0f\n", var_name.c_str(), numa_conflict_ratio);
+				printf ("%s.l1_reuse=%.0f%%\n", var_name.c_str(), l1_reuse);
+				printf ("%s.l2_reuse=%.0f%%\n", var_name.c_str(), l2_reuse);
+				printf ("%s.l3_reuse=%.0f%%\n", var_name.c_str(), l3_reuse);
 			}
 		}
 
