1d147f3a5c41ee19db456e9702d85648e3a24cf2
Leonardo Fialho <leonardofialho@gmail.com>
2013-08-12 12:31:16 -0500

    Adding a function to printf text file to users.

--- tools/common/perfexpert_util.h
+++ tools/common/perfexpert_util.h
@@ -64,7 +64,7 @@ extern "C" {
 #include "perfexpert_output.h"
 #endif
 
-/* perfexpert_util_make_path (create an entire directory tree like 'mkdir -p') */
+/* perfexpert_util_make_path (create directory tree like 'mkdir -p') */
 static int perfexpert_util_make_path(const char *path, int nmode) {
     int oumask;
     char *p = NULL;
@@ -135,7 +135,6 @@ static int perfexpert_util_make_path(const char *path, int nmode) {
     return PERFEXPERT_SUCCESS;
 }
 
-// TODO: add a full path search to these functions
 /* perfexpert_util_file_exists */
 static int perfexpert_util_file_exists(const char *file) {
     if (0 != access(file, F_OK)) {
@@ -145,6 +144,7 @@ static int perfexpert_util_file_exists(const char *file) {
     return PERFEXPERT_SUCCESS;
 }
 
+// TODO: add a full path search to this function
 /* perfexpert_util_file_exists_and_is_executable */
 static int perfexpert_util_file_exists_and_is_exec(const char *file) {
     if (PERFEXPERT_SUCCESS == perfexpert_util_file_exists(file)) {
@@ -162,44 +162,43 @@ static int perfexpert_util_file_exists_and_is_exec(const char *file) {
 /* perfexpert_util_file_copy */
 static int perfexpert_util_file_copy(const char *to, const char *from) {
     int     fd_to, fd_from;
-    char    buf[BUFFER_SIZE];
+    char    buffer[BUFFER_SIZE];
     ssize_t nread;
     int     saved_errno;
 
     fd_from = open(from, O_RDONLY);
-    if (fd_from < 0) {
+    if (0 > fd_from) {
         return PERFEXPERT_ERROR;
     }
 
     fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);
-    if (fd_to < 0) {
+    if (0 > fd_to) {
         goto out_error;
     }
 
-    while (nread = read(fd_from, buf, sizeof buf), nread > 0) {
-        char *out_ptr = buf;
+    while (0 < (nread = read(fd_from, buffer, sizeof(buffer)))) {
+        char *out_ptr = buffer;
         ssize_t nwritten;
 
         do {
             nwritten = write(fd_to, out_ptr, nread);
 
-            if (nwritten >= 0) {
+            if (0 <= nwritten) {
                 nread -= nwritten;
                 out_ptr += nwritten;
             } else if (errno != EINTR) {
                 goto out_error;
             }
-        } while (nread > 0);
+        } while (0 < nread);
     }
 
-    if (nread == 0) {
-        if (close(fd_to) < 0) {
+    if (0 == nread) {
+        if (0 > close(fd_to)) {
             fd_to = -1;
             goto out_error;
         }
         close(fd_from);
 
-        /* Success! */
         return PERFEXPERT_SUCCESS;
     }
 
@@ -207,12 +206,38 @@ static int perfexpert_util_file_copy(const char *to, const char *from) {
     saved_errno = errno;
 
     close(fd_from);
-    if (fd_to >= 0) {
+    if (0 <= fd_to) {
         close(fd_to);
     }
 
     errno = saved_errno;
     return PERFEXPERT_ERROR;
+}
+
+/* perfexpert_util_file_print */
+static int perfexpert_util_file_print(const char *file) {
+    char   buffer[BUFFER_SIZE];
+    FILE   *file_FP;
+    size_t nread;
+
+    if (PERFEXPERT_SUCCESS != perfexpert_util_file_exists(file)) {
+        return PERFEXPERT_ERROR;
+    }
+
+    file_FP = fopen(file, "r");
+    if (file_FP) {
+        while (0 < (nread = fread(buffer, 1, sizeof(buffer), file_FP))) {
+            fwrite(buffer, 1, nread, stdout);
+        }
+        if (ferror(file_FP)) {
+            return PERFEXPERT_ERROR;
+        }
+        fclose(file_FP);
+    } else {
+        return PERFEXPERT_ERROR;
+    }
+
+    return PERFEXPERT_SUCCESS;
 }
 
 #ifdef __cplusplus
