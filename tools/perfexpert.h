a3be2600e775d1021214598777bc007514399ca8
Leonardo Fialho <leonardofialho@gmail.com>
2013-08-20 18:35:11 -0500

    Checking if the Makefile file exists. Improving help message.

--- tools/perfexpert.h
+++ tools/perfexpert.h
@@ -95,7 +95,7 @@ static struct option long_options[] = {
     {"before",        required_argument, NULL, 'b'},
     {"colorful",      no_argument,       NULL, 'c'},
     {"database",      required_argument, NULL, 'd'},
-    {"left-garbage",  no_argument,       NULL, 'g'},
+    {"left_garbage",  no_argument,       NULL, 'g'},
     {"help",          no_argument,       NULL, 'h'},
     {"verbose_level", required_argument, NULL, 'l'},
     {"makefile",      required_argument, NULL, 'm'},
