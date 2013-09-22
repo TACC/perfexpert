8a7ff9710515e12079d3883a72631505f1ae4a70
Leonardo Fialho <leonardofialho@gmail.com>
2013-09-22 13:53:59 -0500

    Clean up.

--- tools/common/perfexpert_list.h
+++ tools/common/perfexpert_list.h
@@ -36,32 +36,6 @@
  * $HEADER$
  */
 
-/**
- * @file 
- *
- * The perfexpert_list interface is used to provide a generic doubly-linked list
- * container for PerfExpert tools. It was inspired by (but is slightly different
- * than) the Open MPI's lists interface, which was inspired by Stantard Template
- * Library (STL) std::list class. One notable difference Open MPI's is that is
- * that here, we provide a small subset of functions. The main difference
- * between Open MPI's lists interface and std::list classe is that when an
- * perfexpert_list_t is destroyed, all of the perfexpert_list_item_t objects
- * that it contains are orphaned -- they are not destroyed.
- *
- * The general idea is that perfexpert_list_item_t can be put on an
- * perfexpert_list_t. Hence, you create a new type that derives from
- * perfexpert_list_item_t; this new type can then be used with perfexpert_list_t
- * containers.
- *
- * NOTE: perfexpert_list_item_t instances can only be on one list at a time.
- * Specifically, if you add an perfexpert_list_item_t to one list, and then add
- * it to another list (without first removing it from the first list), you will
- * effectively be hosing the first list. You have been warned.
- *
- * This list interface does not implement locks. Thus it's is possible the
- * value returned by the perfexpert_list_get_size is incorrect.
- */
-
 #ifndef PERFEXPERT_LIST_H_
 #define PERFEXPERT_LIST_H_
 
@@ -73,95 +47,62 @@ extern "C" {
 #include <stdlib.h>
 #endif
 
-/**
- * Structure of list item and base type for items that are put in list 
- * containers.
- */
+/* Struct of list item and base type for items that are put in lists */
 typedef struct perfexpert_list_item perfexpert_list_item_t;
 struct perfexpert_list_item {
-    /** Pointer to next list item     */
     volatile perfexpert_list_item_t *next;
-    /** Pointer to previous list item */
     volatile perfexpert_list_item_t *prev;
 };
 
-/**
- * Struct of a list and base type for list containers.
- */
-typedef struct perfexpert_list perfexpert_list_t;
-struct perfexpert_list {
-    /** head and tail item of the list                     */
+/* Struct of a list and base type for list containers */
+typedef struct perfexpert_list {
     perfexpert_list_item_t sentinel;
-    /** quick reference to the number of items in the list */
     volatile size_t length;
-};
+} perfexpert_list_t;
 
+/* perfexpert_list_construct */
 static inline void perfexpert_list_construct(perfexpert_list_t *list) {
     list->sentinel.next = &list->sentinel;
     list->sentinel.prev = &list->sentinel;
     list->length = 0;
 }
 
+/* perfexpert_list_destruct */
 static inline void perfexpert_list_destruct(perfexpert_list_t *list) {
     perfexpert_list_construct(list);
 }
 
+/* perfexpert_list_item_construct */
 static inline void perfexpert_list_item_construct(perfexpert_list_item_t *item) {
     item->next = item->prev = NULL;
 }
 
+/* perfexpert_list_item_destruct */
 static inline void perfexpert_list_item_destruct(perfexpert_list_item_t *item) {
     /* nothing to do */
 }
 
-/**
- * Get the next item in a list.
- *
- * @param[in] item   Current list item
- * @retval    item_t The next item in the list
- */
+/* perfexpert_list_get_next */
 #define perfexpert_list_get_next(item) ((item) ? \
     ((perfexpert_list_item_t*) ((perfexpert_list_item_t*)(item))->next) : NULL)
 
-/**
- * Get the previous item in a list.
- *
- * @param[in] item   Current list item
- * @retval    item_t The previous item in the list
- */
+/* perfexpert_list_get_prev */
 #define perfexpert_list_get_prev(item) ((item) ? \
     ((perfexpert_list_item_t*) ((perfexpert_list_item_t*)(item))->prev) : NULL)
 
-/**
- * Return the first item on the list (does not remove it).
- *
- * @param[in] list   The list container
- * @retval    item_t A pointer to the first item on the list
- */
+/* perfexpert_list_get_first */
 static inline perfexpert_list_item_t* perfexpert_list_get_first(
     perfexpert_list_t* list) {
     perfexpert_list_item_t* item = (perfexpert_list_item_t*)list->sentinel.next;
     return item;
 }
 
-/**
- * Return the number of items in a list
- *
- * @param[in] list   The list container
- * @retval    size_t The size of the list
- */
+/* perfexpert_list_get_size */
 static inline size_t perfexpert_list_get_size(perfexpert_list_t* list) {
     return list->length;
 }
 
-/**
- * Remove an item from a list.
- *
- * @param[in] list   The list container
- * @param[in] item   The item to remove
- * @retval    item_t A pointer to the item on the list previous to the one that
- *                   was removed.
- */
+/* perfexpert_list_remove_item */
 static inline perfexpert_list_item_t *perfexpert_list_remove_item(
     perfexpert_list_t *list, perfexpert_list_item_t *item) {
     item->prev->next = item->next;
@@ -171,25 +112,14 @@ static inline perfexpert_list_item_t *perfexpert_list_remove_item(
     return (perfexpert_list_item_t *)item->prev;
 }
 
-/**
- * Append an item to the end of the list.
- *
- * @param[in] list The list container
- * @param[in] item The item to append
- */
+/* perfexpert_list_append */
 static inline void perfexpert_list_append(perfexpert_list_t *list,
     perfexpert_list_item_t *item) {
     perfexpert_list_item_t *sentinel = &(list->sentinel);
-
-    /* set new element's previous pointer             */
     item->prev = sentinel->prev;
-    /* reset previous pointer on current last element */
     sentinel->prev->next = item;
-    /* reset new element's next pointer               */
     item->next = sentinel;
-    /* reset the list's tail element previous pointer */
     sentinel->prev = item;
-    /* increment list element counter */
     list->length++;
 }
 
