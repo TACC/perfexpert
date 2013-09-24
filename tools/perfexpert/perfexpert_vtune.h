d64bee085913eab985fea301dd86d58571d67c29
Leonardo Fialho <leonardofialho@gmail.com>
2013-09-24 13:49:35 -0500

    Adding support to collect measurement using VTune Amplifier XE.

--- tools/perfexpert/perfexpert_vtune.h
+++ tools/perfexpert/perfexpert_vtune.h
@@ -32,6 +32,12 @@
 extern "C" {
 #endif
 
+#define VTUNE_EXPERIMENT_FILE     "experiment_vtune.conf"
+#define VTUNE_MIC_EXPERIMENT_FILE "experiment_vtune_mic.conf"
+
+/* HPCToolkit stuff (binaries should be in the path) */
+#define VTUNE_AMPLIFIER "amplxe-cl"
+
 /* Function declarations */
 int measurements_vtune(void);
 
