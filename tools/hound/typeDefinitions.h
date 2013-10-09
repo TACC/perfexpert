7b414a3b2075bd79bc4183e4e5314c1b7eba29aa
Leonardo Fialho <leonardofialho@gmail.com>
2013-10-09 13:12:19 -0500

    General licencse update.

--- tools/hound/typeDefinitions.h
+++ tools/hound/typeDefinitions.h
@@ -1,22 +1,22 @@
 /*
- * Copyright (C) 2013 The University of Texas at Austin
+ * Copyright (c) 2011-2013  University of Texas at Austin. All rights reserved.
+ *
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
  *
  * This file is part of PerfExpert.
  *
  * PerfExpert is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
+ * the terms of the The University of Texas at Austin Research License
+ * 
  * PerfExpert is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * A PARTICULAR PURPOSE.
+ * 
+ * Authors: Leonardo Fialho and Ashay Rane
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with PerfExpert. If not, see <http://www.gnu.org/licenses/>.
- *
- * Author: Ashay Rane
+ * $HEADER$
  */
 
 #ifndef TYPEDEFINITIONS_H_
