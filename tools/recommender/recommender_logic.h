ca7756453d5279afb7d0129193759be593b7098b
Leonardo Fialho <leonardofialho@gmail.com>
2013-11-02 14:22:55 -0500

    Updating recommender tool according to the new code structure

--- tools/recommender/recommender_logic.h
+++ tools/recommender/recommender_logic.h
@@ -9,48 +9,39 @@
  *
  * PerfExpert is free software: you can redistribute it and/or modify it under
  * the terms of the The University of Texas at Austin Research License
- * 
+ *
  * PerfExpert is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE.
- * 
+ *
  * Authors: Leonardo Fialho and Ashay Rane
  *
  * $HEADER$
  */
 
+#ifndef RECOMMENDER_LOGIC_H_
+#define RECOMMENDER_LOGIC_H_
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* System standard headers */
-#include <string.h>
+/* Structure to hold strategies to select recommendations */
+typedef struct {
+    volatile perfexpert_list_item_t *next;
+    volatile perfexpert_list_item_t *prev;
+    char *name;
+    char *query;
+} strategy_t;
 
-/* PerfExpert headers */
-#include "recommender.h"
-#include "perfexpert_log.h"
-
-/* log_bottleneck */
-int log_bottleneck(void *var, int count, char **val, char **names) {
-    char temp_str[MAX_LOG_ENTRY];
-    int  i;
-
-    bzero(temp_str, MAX_LOG_ENTRY);
-    for (i = 0; i < count; i++) {
-        if (NULL != val[i]) {
-            strcat(temp_str, names[i]);
-            strcat(temp_str, "=");
-            strcat(temp_str, val[i]);
-            strcat(temp_str, ";");
-        }
-    }
-    LOG(("%s", temp_str));
-
-    return PERFEXPERT_SUCCESS;
-}
+/* Function declarations */
+static int accumulate_strategies(void *strategies, int c, char **val,
+    char **names);
+static int select_recom_hotspot(void *var, int c, char **val, char **names);
+static int print_recom(void *var, int count, char **val, char **names);
 
 #ifdef __cplusplus
 }
 #endif
 
-// EOF
+#endif /* RECOMMENDER_LOGIC_H */
--- tools/recommender/recommender_options.c
+++ tools/recommender/recommender_options.c
@@ -9,11 +9,11 @@
  *
  * PerfExpert is free software: you can redistribute it and/or modify it under
  * the terms of the The University of Texas at Austin Research License
- * 
+ *
  * PerfExpert is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE.
- * 
+ *
  * Authors: Leonardo Fialho and Ashay Rane
  *
  * $HEADER$
@@ -26,100 +26,22 @@ extern "C" {
 /* System standard headers */
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
+#include <argp.h>
+#include <math.h>
 
 /* PerfExpert headers */
 #include "recommender.h"
-#include "perfexpert_constants.h"
-#include "perfexpert_output.h"
-#include "install_dirs.h"
+#include "recommender_options.h"
 
-/* Structure to handle command line arguments. Try to keep the content of
- * this structure compatible with the parse_cli_params() and show_help().
- */
-static struct option long_options[] = {
-    {"colorful",        no_argument,       NULL, 'c'},
-    {"database",        required_argument, NULL, 'd'},
-    {"help",            no_argument,       NULL, 'h'},
-    {"inputfile",       required_argument, NULL, 'i'},
-    {"metricsfile",     required_argument, NULL, 'm'},
-    {"outputfile",      required_argument, NULL, 'o'},
-    {"pid",             required_argument, NULL, 'p'},
-    {"recommendations", required_argument, NULL, 'r'},
-    {"verbose",         optional_argument, NULL, 'v'},
-    {"workdir",         required_argument, NULL, 'w'},
-    {0, 0, 0, 0}
-};
+/* PerfExpert common headers */
+#include "common/perfexpert_constants.h"
+#include "common/perfexpert_output.h"
 
-/* show_help */
-void show_help(void) {
-    OUTPUT_VERBOSE((10, "printing help"));
-    /*      12345678901234567890123456789012345678901234567890123456789012345678901234567890 */
-    printf("Usage: recommender [-cdhimoprvw]\n");
-    printf("  -c --colorful        Enable colors on verbose mode, no weird characters will\n");
-    printf("                       appear on output files\n");
-    printf("  -d --database        Select the recommendation database file\n");
-    printf("                       (default: %s/%s)\n", PERFEXPERT_VARDIR, PERFEXPERT_DB);
-    printf("  -h --help            Show this message\n");
-    printf("  -i --inputfile       Set input file for performance metrics\n");
-    printf("  -m --metricsfile     Set input file for user-defined\n");
-    printf("  -o --outputfile      Set output file for recommendations (default: STDOUT)\n");
-    printf("  -p --pid             Set procedd ID for logging purposes\n");
-    printf("  -r --recommendations Number of recommendation to show\n");
-    printf("  -v --verbose         Enable verbose mode (default: disabled if not set, 5 if\n");
-    printf("                       set, valid options: 1-10)\n");
-    printf("  -w --workdir         Directory where output files should be created\n");
-    printf("                       (default: current directory)\n");
-}
-
-/* parse_env_vars */
-int parse_env_vars(void) {
-    if ((NULL != getenv("PERFEXPERT_RECOMMENDER_COLORFUL")) &&
-        (1 == atoi(getenv("PERFEXPERT_RECOMMENDER_COLORFUL")))) {
-        globals.colorful = 1;
-        OUTPUT_VERBOSE((5, "ENV: colorful=YES"));
-    }
-    if (NULL != getenv("PERFEXPERT_RECOMMENDER_DATABASE_FILE")) {
-        globals.dbfile =  getenv("PERFEXPERT_RECOMMENDER_DATABASE_FILE");
-        OUTPUT_VERBOSE((5, "ENV: dbfile=%s", globals.dbfile));
-    }
-    // No variable for -h --help
-    if (NULL != getenv("PERFEXPERT_RECOMMENDER_INPUT_FILE")) {
-        globals.inputfile = getenv("PERFEXPERT_RECOMMENDER_INPUT_FILE");
-        OUTPUT_VERBOSE((5, "ENV: inputfile=%s", globals.inputfile));
-    }
-    // No variable for -m --metricsfile
-    if (NULL != getenv("PERFEXPERT_RECOMMENDER_OUTPUT_FILE")) {
-        globals.outputfile = getenv("PERFEXPERT_RECOMMENDER_OUTPUT_FILE");
-        OUTPUT_VERBOSE((5, "ENV: outputfile=%s", globals.outputfile));
-    }
-    if (NULL != getenv("PERFEXPERT_RECOMMENDER_PID")) {
-        globals.pid = atoi(getenv("PERFEXPERT_RECOMMENDER_PID"));
-        OUTPUT_VERBOSE((5, "ENV: pid=%d", globals.pid));
-    }
-    if (NULL != getenv("PERFEXPERT_RECOMMENDER_REC_COUNT")) {
-        globals.rec_count = atoi(getenv("PERFEXPERT_RECOMMENDER_REC_COUNT"));
-        OUTPUT_VERBOSE((5, "ENV: rec_count=%d", globals.rec_count));
-    }
-    if (NULL != getenv("PERFEXPERT_RECOMMENDER_VERBOSE_LEVEL")) {
-        globals.verbose = atoi(getenv("PERFEXPERT_RECOMMENDER_VERBOSE_LEVEL"));
-        OUTPUT_VERBOSE((5, "ENV: verbose_level=%d", globals.verbose));
-    }
-    if (NULL != getenv("PERFEXPERT_RECOMMENDER_WORKDIR")) {
-        globals.workdir = getenv("PERFEXPERT_RECOMMENDER_WORKDIR");
-        OUTPUT_VERBOSE((5, "ENV: workdir=%s", globals.workdir));
-    }
-    if (NULL != getenv("PERFEXPERT_RECOMMENDER_METRICS_FILE")) { // Not -m
-        globals.outputmetrics = getenv("PERFEXPERT_RECOMMENDER_METRICS_FILE");
-        OUTPUT_VERBOSE((5, "ENV: metrics_file=%s", globals.outputmetrics));
-    }
-    return PERFEXPERT_SUCCESS;
-}
+static struct argp argp = { options, parse_options, args_doc, doc };
 
 /* parse_cli_params */
 int parse_cli_params(int argc, char *argv[]) {
-    int parameter = 0;    /* Temporary variable to hold parameter */
-    int option_index = 0; /* getopt_long() stores the option index here */
+    int i = 0;
 
     /* If some environment variable is defined, use it! */
     if (PERFEXPERT_SUCCESS != parse_env_vars()) {
@@ -127,92 +49,35 @@ int parse_cli_params(int argc, char *argv[]) {
         return PERFEXPERT_ERROR;
     }
 
-    while (1) {
-        /* get parameter */
-        parameter = getopt_long(argc, argv, "cd:hi:m:o:p:r:v:w:",
-            long_options, &option_index);
+    /* Parse arguments */
+    argp_parse(&argp, argc, argv, 0, 0, &globals);
 
-        /* Detect the end of the options */
-        if (-1 == parameter) {
-            break;
-        }
+    /* Sanity check: verbose level should be between 1-10 */
+    if ((0 > globals.verbose) || (10 < globals.verbose)) {
+        OUTPUT(("%s", _ERROR("Error: invalid verbose level")));
+        return PERFEXPERT_ERROR;
+    }
 
-        switch (parameter) {
-            /* Activate colorful mode */
-            case 'c':
-                globals.colorful = PERFEXPERT_TRUE;
-                OUTPUT_VERBOSE((10, "option 'c' set"));
-                break;
-            /* Which database file? */
-            case 'd':
-                globals.dbfile = optarg;
-                OUTPUT_VERBOSE((10, "option 'd' set [%s]", globals.dbfile));
-                break;
-            /* Show help */
-            case 'h':
-                OUTPUT_VERBOSE((10, "option 'h' set"));
-                show_help();
-                return PERFEXPERT_ERROR;
-            /* Use input file? */
-            case 'i':
-                globals.inputfile = optarg;
-                OUTPUT_VERBOSE((10, "option 'i' set [%s]", globals.inputfile));
-                break;
-            /* Specify new metrics */
-            case 'm':
-                globals.metrics_file = optarg;
-                OUTPUT_VERBOSE((10, "option 'm' set [%s]",
-                    globals.metrics_file));
-                break;
-            /* Use output file? */
-            case 'o':
-                globals.outputfile = optarg;
-                OUTPUT_VERBOSE((10, "option 'o' set [%s]", globals.outputfile));
-                break;
-            /* Specify PID */
-            case 'p':
-                globals.pid = atoi(optarg);
-                OUTPUT_VERBOSE((10, "option 'p' set [%d]", globals.pid));
-                break;
-            /* Number of recommendation to output */
-            case 'r':
-                globals.rec_count = atoi(optarg);
-                OUTPUT_VERBOSE((10, "option 'r' set [%d]", globals.rec_count));
-                break;
-            /* Verbose */
-            case 'v':
-                globals.verbose = atoi(optarg);
-                OUTPUT_VERBOSE((10, "option 'v' set [%d]", globals.verbose));
-                break;
-            /* Workdir */
-            case 'w':
-                globals.workdir = optarg;
-                OUTPUT_VERBOSE((10, "option 'w' set [%s]", globals.workdir));
-                break;
-            /* Unknown option */
-            case '?':
-            default:
-                show_help();
-                return PERFEXPERT_ERROR;
-        }
+    /* Sanity check: unique ID is mandatory */
+    if (0 == globals.uid) {
+        OUTPUT(("%s", _ERROR("Error: undefined uid")));
+        return PERFEXPERT_ERROR;
     }
 
     /* Print summary */
     OUTPUT_VERBOSE((7, "%s", _BLUE("Summary of options")));
-    OUTPUT_VERBOSE((10, "   Colorful verbose?  %s", globals.colorful ? "yes" : "no"));
-    OUTPUT_VERBOSE((10, "   Database file:     %s", globals.dbfile));
-    OUTPUT_VERBOSE((10, "   Input file:        %s", globals.inputfile));
-    OUTPUT_VERBOSE((10, "   Metrics file:      %s", globals.metrics_file));
-    OUTPUT_VERBOSE((10, "   Output file:       %s", globals.outputfile));
-    OUTPUT_VERBOSE((10, "   PID:               %d", globals.pid));
-    OUTPUT_VERBOSE((10, "   Recommendations:   %d", globals.rec_count));
-    OUTPUT_VERBOSE((10, "   Verbose level:     %d", globals.verbose));
-    OUTPUT_VERBOSE((10, "   Work directory:    %s", globals.workdir));
-    OUTPUT_VERBOSE((10, "   Output metrics:    %s", globals.outputmetrics));
+    OUTPUT_VERBOSE((7, "   Verbose level:      %d", globals.verbose));
+    OUTPUT_VERBOSE((7, "   Colorful verbose?   %s", globals.colorful ?
+        "yes" : "no"));
+    OUTPUT_VERBOSE((7, "   Database file:      %s", globals.dbfile));
+    OUTPUT_VERBOSE((7, "   UID:                %llu", globals.uid));
+    OUTPUT_VERBOSE((7, "   Recommendations:    %d", globals.rec_count));
+    OUTPUT_VERBOSE((7, "   Output file:        %s", globals.outputfile));
+    OUTPUT_VERBOSE((7, "   Output transformer: %s", globals.outputmetrics));
+    OUTPUT_VERBOSE((7, "   Work directory:     %s", globals.workdir));
 
     /* Not using OUTPUT_VERBOSE because I want only one line */
-    if (8 <= globals.verbose) {
-        int i;
+    if (7 <= globals.verbose) {
         printf("%s    %s", PROGRAM_PREFIX, _YELLOW("command line:"));
         for (i = 0; i < argc; i++) {
             printf(" %s", argv[i]);
@@ -220,18 +85,127 @@ int parse_cli_params(int argc, char *argv[]) {
         printf("\n");
     }
 
-    /* Sanity check: input file is mandatory */
-    if (NULL == globals.inputfile) {
-        OUTPUT(("%s", _ERROR("Error: undefined input file")));
-        show_help();
-        return PERFEXPERT_ERROR;
+    return PERFEXPERT_SUCCESS;
+}
+
+/* parse_options */
+static error_t parse_options(int key, char *arg, struct argp_state *state) {
+    switch (key) {
+
+        /* Activate colorful mode */
+        case 'c':
+            globals.colorful = PERFEXPERT_TRUE;
+            OUTPUT_VERBOSE((1, "option 'c' set"));
+            break;
+
+        /* Database */
+        case 'd':
+            globals.dbfile = arg;
+            OUTPUT_VERBOSE((1, "option 'd' set [%s]", globals.dbfile));
+            break;
+
+        /* Show help */
+        case 'h':
+            OUTPUT_VERBOSE((1, "option 'h' set"));
+            argp_state_help(state, stdout, ARGP_HELP_STD_HELP);
+            break;
+
+        /* Verbose level (has an alias: v) */
+        case 'l':
+            globals.verbose = arg ? atoi(arg) : 5;
+            OUTPUT_VERBOSE((1, "option 'l' set [%d]", globals.verbose));
+            break;
+
+        /* Output file */
+        case 'o':
+            globals.outputfile = arg;
+            OUTPUT_VERBOSE((1, "option 'o' set [%s]", globals.outputfile));
+            break;
+
+        /* Number of recommendation to output */
+        case 'r':
+            globals.rec_count = atoi(arg);
+            OUTPUT_VERBOSE((1, "option 'r' set [%d]", globals.rec_count));
+            break;
+
+        /* Output for code transformer */
+        case 't':
+            globals.outputmetrics = arg;
+            OUTPUT_VERBOSE((1, "option 't' set [%d]", globals.outputmetrics));
+            break;
+
+        /* Unique ID */
+        case 'u':
+            globals.uid = strtoll(arg, NULL, 10);
+            OUTPUT_VERBOSE((1, "option 't' set [%llu]", globals.uid));
+            break;
+
+        /* Verbose level (has an alias: l) */
+        case 'v':
+            globals.verbose = arg ? atoi(arg) : 5;
+            OUTPUT_VERBOSE((1, "option 'v' set [%d]", globals.verbose));
+            break;
+
+        /* Workdir */
+        case 'w':
+            globals.workdir = optarg;
+            OUTPUT_VERBOSE((10, "option 'w' set [%s]", globals.workdir));
+            break;
+
+        /* ... */
+        case ARGP_KEY_ARG:
+        case ARGP_KEY_NO_ARGS:
+        case ARGP_KEY_END:
+            break;
+
+        /* Unknown option */
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+/* parse_env_vars */
+static int parse_env_vars(void) {
+    if ((NULL != getenv("PERFEXPERT_RECOMMENDER_COLORFUL")) &&
+        (1 == atoi(getenv("PERFEXPERT_RECOMMENDER_COLORFUL")))) {
+        globals.colorful = 1;
+        OUTPUT_VERBOSE((5, "ENV: colorful=YES"));
     }
 
-    /* Sanity check: verbose level should be between 1-10 */
-    if ((0 > globals.verbose) || (10 < globals.verbose)) {
-        OUTPUT(("%s", _ERROR("Error: invalid verbose level")));
-        show_help();
-        return PERFEXPERT_ERROR;
+    if (NULL != getenv("PERFEXPERT_RECOMMENDER_DATABASE")) {
+        globals.dbfile =  getenv("PERFEXPERT_RECOMMENDER_DATABASE");
+        OUTPUT_VERBOSE((5, "ENV: dbfile=%s", globals.dbfile));
+    }
+
+    if (NULL != getenv("PERFEXPERT_RECOMMENDER_OUTPUT_FILE")) {
+        globals.outputfile = getenv("PERFEXPERT_RECOMMENDER_OUTPUT_FILE");
+        OUTPUT_VERBOSE((5, "ENV: outputfile=%s", globals.outputfile));
+    }
+
+    if (NULL != getenv("PERFEXPERT_RECOMMENDER_UID")) {
+        globals.uid = strtoll(getenv("PERFEXPERT_RECOMMENDER_UID"), NULL, 10);
+        OUTPUT_VERBOSE((5, "ENV: uid=%llu", globals.uid));
+    }
+
+    if (NULL != getenv("PERFEXPERT_RECOMMENDER_REC_COUNT")) {
+        globals.rec_count = atoi(getenv("PERFEXPERT_RECOMMENDER_REC_COUNT"));
+        OUTPUT_VERBOSE((5, "ENV: rec_count=%d", globals.rec_count));
+    }
+
+    if (NULL != getenv("PERFEXPERT_RECOMMENDER_VERBOSE_LEVEL")) {
+        globals.verbose = atoi(getenv("PERFEXPERT_RECOMMENDER_VERBOSE_LEVEL"));
+        OUTPUT_VERBOSE((5, "ENV: verbose_level=%d", globals.verbose));
+    }
+
+    if (NULL != getenv("PERFEXPERT_RECOMMENDER_WORKDIR")) {
+        globals.workdir = getenv("PERFEXPERT_RECOMMENDER_WORKDIR");
+        OUTPUT_VERBOSE((5, "ENV: workdir=%s", globals.workdir));
+    }
+
+    if (NULL != getenv("PERFEXPERT_RECOMMENDER_METRICS_FILE")) {
+        globals.outputmetrics = getenv("PERFEXPERT_RECOMMENDER_METRICS_FILE");
+        OUTPUT_VERBOSE((5, "ENV: metrics_file=%s", globals.outputmetrics));
     }
 
     return PERFEXPERT_SUCCESS;
