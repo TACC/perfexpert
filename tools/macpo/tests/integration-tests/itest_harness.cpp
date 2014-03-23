01b2ea7f4d4358440283fca213efbd262e9cd8c5
Ashay Rane <ashay.r@gmail.com>
2014-03-22 20:33:13 -0500

    Checking for remaining expected outputs that was not seen in program output.

--- tools/macpo/tests/integration-tests/itest_harness.cpp
+++ tools/macpo/tests/integration-tests/itest_harness.cpp
@@ -135,7 +135,13 @@ bool verify_output(std::string& filename, std::string& binary) {
         }
     }
 
-    return true;
+    // Are there any expected outputs that
+    // we did not see in the output?
+    if (ctr == file_lines || (ctr < file_lines &&
+            file_line_list[ctr].compare(0, expected_prefix.size(),
+                expected_prefix) != 0)) {
+        return true;
+    }
 }
 
 static void populate_args(string_list_t& args, std::string& input_file,
