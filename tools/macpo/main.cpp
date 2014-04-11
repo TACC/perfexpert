b2f8a2ee64b6adf7c49d22ac5bca4ace152fed5b
Ashay Rane <ashay.r@gmail.com>
2014-04-11 16:39:01 -0500

    Check exceptions when calling Rose.
    This patch moves calls to the Rose frontend, instrumentation classes and the
    backend to the umbrella exception-checking code.

--- tools/macpo/main.cpp
+++ tools/macpo/main.cpp
@@ -28,6 +28,42 @@
 #include "argparse.h"
 #include "minst.h"
 
+bool midend(SgProject* project, options_t& options) {
+    SgFilePtrList files = project->get_fileList();
+    if (options.backup_filename.size()) {
+        // We need to save the input file to a backup file.
+        if (files.size() != 1) {
+            std::cerr << mprefix << "Backup option can be specified with only "
+                << "a single file for compilation, terminating." << std::endl;
+            return false;
+        }
+
+        SgSourceFile* file = isSgSourceFile(*(files.begin()));
+        std::string source = file->get_file_info()->get_filenameString();
+
+        // Copy the file over.
+        if (argparse::copy_file(source.c_str(),
+                options.backup_filename.c_str()) < 0) {
+            std::cerr << mprefix << "Error backing up file." << std::endl;
+            return false;
+        }
+
+        std::cerr << mprefix << "Saved " << source << " into " <<
+            options.backup_filename << "." << std::endl;
+    }
+
+    // Loop over each file
+    for (SgFilePtrList::iterator it = files.begin(); it != files.end(); it++) {
+        SgSourceFile* file = isSgSourceFile(*it);
+
+        // Start the traversal!
+        MINST traversal(options, project);
+        traversal.traverseWithinFile(file, preorder);
+    }
+
+    return true;
+}
+
 int main(int argc, char *argv[]) {
     options_t options;
     argparse::init_options(options);
@@ -58,63 +94,35 @@ int main(int argc, char *argv[]) {
     if (options.action != ACTION_NONE && options.function_name.size() == 0) {
         std::cerr << mprefix << "USAGE: " << argv[0] << " <options>" <<
             std::endl;
-        std::cerr << mprefix << "Did not find valid options on the command "
-            << "line" << std::endl;
+        std::cerr << mprefix << "Did not find valid options on the command line"
+            << std::endl;
         return -1;
     }
 
-    SgProject *project = frontend(arguments);
+    try {
+        SgProject *project = project = frontend(arguments);
+        ROSE_ASSERT(project != NULL);
 
-    // Check if we have explicit instructions not to change anything.
-    if (options.action == ACTION_NONE) {
+        // Check if we really need to invoke the instrumentation.
+        if (options.action != ACTION_NONE) {
+            if (midend(project, options) == false) {
+                return -1;
+            }
+        }
+
         if (!options.no_compile) {
+            // FIXME: ROSE tests seem to be broken,
+            // operand of AddressOfOp is not allowed to be an l-value.
+            // AstTests::runAllTests(project);
             return backend(project) == 0 ? 0 : 1;
         }
 
         project->unparse();
-        return 0;
+    } catch (rose_exception e) {
+        // The frontend() call will print messages to the console,
+        // hence no other error message needs to be printed.
+        return -1;
     }
 
-    SgFilePtrList files = project->get_fileList();
-    if (options.backup_filename.size()) {
-        // We need to save the input file to a backup file.
-        if (files.size() != 1) {
-            std::cerr << mprefix << "Backup option can be specified with "
-                << "only a single file for compilation, terminating." <<
-                std::endl;
-            return -1;
-        }
-
-        SgSourceFile* file = isSgSourceFile(*(files.begin()));
-        std::string source = file->get_file_info()->get_filenameString();
-
-        // Copy the file over.
-        if (argparse::copy_file(source.c_str(),
-                    options.backup_filename.c_str()) < 0) {
-            std::cerr << mprefix << "Error backing up file." << std::endl;
-            return -1;
-        }
-
-        std::cerr << mprefix << "Saved " << source << " into " <<
-            options.backup_filename << "." << std::endl;
-    }
-
-    // Loop over each file
-    for (SgFilePtrList::iterator it = files.begin(); it != files.end(); it++) {
-        SgSourceFile* file = isSgSourceFile(*it);
-
-        // Start the traversal!
-        MINST traversal(options, project);
-        traversal.traverseWithinFile(file, preorder);
-    }
-
-    if (!options.no_compile) {
-        // FIXME: ROSE tests seem to be broken,
-        // operand of AddressOfOp is not allowed to be an l-value.
-        // AstTests::runAllTests(project);
-        return backend(project) == 0 ? 0 : 1;
-    }
-
-    project->unparse();
     return 0;
 }
