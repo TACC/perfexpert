29193460699100ffd1d19a616e5a41a962490db4
Ashay Rane <ashay.r@gmail.com>
2014-10-25 22:27:39 -0500

    Bugfix: Reuse distance counting.
    This patch fixes an issue with the reuse distance counting, whereby the
    distance is calculated as the number of tree nodes that were added after a
    specific node was added to the tree. This patch does not completely solve the
    problem, though -- as the underlying AVL tree code does not support node
    deletion. Including this support should reduce the number of passes that need
    to be made over nodes that represent references.
    
    Further reduction in the passes should be possible from recording the number of
    nodes in any given subtree. At the moment, the code walks over subtrees to
    count nodes. In combination with the previous optimizations, the true benefit
    of using AVL trees for counting reuse distances should be possible.

--- tools/macpo/common/avl_tree.h
+++ tools/macpo/common/avl_tree.h
@@ -29,6 +29,7 @@
 #include <stdint.h>
 
 #include <cassert>
+#include <cstdlib>
 #include <iostream>
 #include <map>
 
@@ -62,10 +63,26 @@ class avl_tree {
         std::map<size_t, avl_node_t*>::iterator it =
             addr_to_node.find(cache_line);
         if (it != addr_to_node.end()) {
-            avl_node_t* node = addr_to_node[cache_line];
-            if (node) {
+            avl_node_t* node = it->second;
+            if (node && node->valid) {
                 assert(ADDR_TO_CACHE_LINE(node->data.address) == cache_line);
-                return last_key - node->key - 1;
+                uint64_t count = node_count(node->right);
+                while (node != NULL && node != root) {
+                    avl_node_t* parent = node->parent;
+                    assert(parent != NULL && "Invalid parent pointer!");
+                    if (parent->left == node) {
+                        assert(parent->right != node && "Invalid parent ptr!");
+
+                        // node is left child of parent.
+                        count += node_count(parent->right);
+                    } else {
+                        assert(parent->right == node && "Invalid parent ptr!");
+                    }
+
+                    node = parent;
+                }
+
+                return count;
             }
         }
 
@@ -77,9 +94,9 @@ class avl_tree {
         std::map<size_t, avl_node_t*>::iterator it =
             addr_to_node.find(cache_line);
         if (it != addr_to_node.end()) {
-            avl_node_t* node = addr_to_node[cache_line];
+            avl_node_t* node = it->second;
             if (node) {
-                node->key = last_key - distance - 1;
+                node->valid = false;
             }
         }
     }
@@ -87,17 +104,22 @@ class avl_tree {
     bool insert(const mem_info_t *data_ptr, int64_t delta = 0) {
         if (data_ptr) {
             avl_node_t* node_to_insert = NULL;
-            node_to_insert = (avl_node_t*) malloc(sizeof(avl_node_t));
+            void* ptr = malloc(sizeof(avl_node_t));
+            node_to_insert = static_cast<avl_node_t*>(ptr);
+
             if (node_to_insert) {
                 node_to_insert->data = *data_ptr;
                 node_to_insert->height = 0;
+                node_to_insert->valid = true;
                 node_to_insert->key = last_key - delta;
+                node_to_insert->parent = NULL;
                 node_to_insert->left = node_to_insert->right = NULL;
 
                 avl_node_t* node = _insert(&root, node_to_insert);
                 if (node) {
-                    addr_to_node[ADDR_TO_CACHE_LINE(data_ptr->address)] =
-                        node_to_insert;
+                    size_t cache_line = ADDR_TO_CACHE_LINE(data_ptr->address);
+                    update_addr(cache_line, node_to_insert);
+
                     last_key += 1;
                     return true;
                 }
@@ -112,7 +134,8 @@ class avl_tree {
         int height;
         mem_info_t data;
         size_t key;
-        struct tag_avl_node *left, *right;
+        bool valid;
+        struct tag_avl_node *left, *right, *parent;
     } avl_node_t;
 
     void _print(avl_node_t* n) {
@@ -136,6 +159,22 @@ class avl_tree {
         _print(n->right);
     }
 
+    void update_addr(size_t cache_line, avl_node_t* node) {
+        std::map<size_t, avl_node_t*>::iterator it =
+            addr_to_node.find(cache_line);
+        if (it == addr_to_node.end()) {
+            addr_to_node[cache_line] = node;
+            node->valid = true;
+            return;
+        }
+
+        avl_node_t* old_node = it->second;
+        old_node->valid = false;
+
+        addr_to_node[cache_line] = node;
+        node->valid = true;
+    }
+
     int height(avl_node_t* n) {
         if (n == NULL) {
             return -1;
@@ -152,37 +191,56 @@ class avl_tree {
         }
     }
 
+    uint64_t node_count(avl_node_t* subtree_root) {
+        if (subtree_root == NULL) {
+            return 0;
+        }
+
+        uint64_t count = 0;
+        count += node_count(subtree_root->left);
+        count += node_count(subtree_root->right);
+
+        if (subtree_root->valid) {
+            count += 1;
+        }
+
+        return count;
+    }
+
     avl_node_t* _insert(avl_node_t** t, avl_node_t* node_to_insert) {
         if (*t == NULL) {
             *t = node_to_insert;
         } else if (node_to_insert->key < (*t)->key) {
             (*t)->left = _insert(&((*t)->left), node_to_insert);
-            if (height((*t)->left) - height((*t)->right) == 2)
+            if (height((*t)->left) - height((*t)->right) == 2) {
                 if (node_to_insert->key < (*t)->left->key) {
                     *t = single_rotate_with_left(*t);
                 } else {
                     *t = double_rotate_with_left(*t);
                 }
+            }
+
+            (*t)->left->parent = *t;
         } else if (node_to_insert->key > (*t)->key) {
             (*t)->right = _insert(&((*t)->right), node_to_insert);
-            if (height((*t)->right) - height((*t)->left) == 2)
+            if (height((*t)->right) - height((*t)->left) == 2) {
                 if (node_to_insert->key > (*t)->right->key) {
                     *t = single_rotate_with_right(*t);
                 } else {
                     *t = double_rotate_with_right(*t);
                 }
+            }
+
+            (*t)->right->parent = *t;
         }
 
         (*t)->height = MAX(height((*t)->left), height((*t)->right)) + 1;
         return *t;
     }
 
-
     avl_node_t* double_rotate_with_left(avl_node_t* k3) {
         /* Rotate between k1 and k2 */
         k3->left = single_rotate_with_right(k3->left);
-
-        // if (k3->left)   addr_to_node[k3->left->data.address] = k3->left;
 
         /* Rotate between K3 and k2 */
         return single_rotate_with_left(k3);
@@ -191,8 +249,6 @@ class avl_tree {
     avl_node_t* double_rotate_with_right(avl_node_t* k1) {
         /* rotate between K3 and k2 */
         k1->right = single_rotate_with_left(k1->right);
-
-        // if (k1->right)  addr_to_node[k1->right->data.address] = k1->right;
 
         /* rotate between k1 and k2 */
         return single_rotate_with_right(k1);
@@ -205,21 +261,35 @@ class avl_tree {
         k2->left = k1->right;
         k1->right = k2;
 
+        k2->parent = k1;
+        if (k2->left != NULL) {
+            k2->left->parent = k2;
+        }
+
         k2->height = MAX(height(k2->left), height(k2->right)) + 1;
         k1->height = MAX(height(k1->left), k2->height) + 1;
 
-        if (k1)         addr_to_node[ADDR_TO_CACHE_LINE(k1->data.address)] = k1;
-        if (k2)         addr_to_node[ADDR_TO_CACHE_LINE(k2->data.address)] = k2;
+        if (k1) {
+            size_t cache_line = ADDR_TO_CACHE_LINE(k1->data.address);
+            update_addr(cache_line, k1);
+        }
+
+        if (k2) {
+            size_t cache_line = ADDR_TO_CACHE_LINE(k2->data.address);
+            update_addr(cache_line, k2);
+        }
 
         avl_node_t* k2l = k2->left;
         avl_node_t* k1r = k1->right;
 
         if (k1r) {
-            addr_to_node[ADDR_TO_CACHE_LINE(k1r->data.address)] = k1r;
+            size_t cache_line = ADDR_TO_CACHE_LINE(k1r->data.address);
+            update_addr(cache_line, k1r);
         }
 
         if (k2l) {
-            addr_to_node[ADDR_TO_CACHE_LINE(k2l->data.address)] = k2l;
+            size_t cache_line = ADDR_TO_CACHE_LINE(k2l->data.address);
+            update_addr(cache_line, k2l);
         }
 
         return k1; /* new root */
@@ -232,21 +302,35 @@ class avl_tree {
         k1->right = k2->left;
         k2->left = k1;
 
+        k2->left->parent = k2;
+        if (k1->right != NULL) {
+            k1->right->parent = k1;
+        }
+
         k1->height = MAX(height(k1->left), height(k1->right)) + 1;
         k2->height = MAX(height(k2->right), k1->height) + 1;
 
-        if (k1)         addr_to_node[ADDR_TO_CACHE_LINE(k1->data.address)] = k1;
-        if (k2)         addr_to_node[ADDR_TO_CACHE_LINE(k2->data.address)] = k2;
+        if (k1) {
+            size_t cache_line = ADDR_TO_CACHE_LINE(k1->data.address);
+            update_addr(cache_line, k1);
+        }
+
+        if (k2) {
+            size_t cache_line = ADDR_TO_CACHE_LINE(k2->data.address);
+            update_addr(cache_line, k2);
+        }
 
         avl_node_t* k2l = k2->left;
         avl_node_t* k1r = k1->right;
 
         if (k1r) {
-            addr_to_node[ADDR_TO_CACHE_LINE(k1r->data.address)] = k1r;
+            size_t cache_line = ADDR_TO_CACHE_LINE(k1r->data.address);
+            update_addr(cache_line, k1r);
         }
 
         if (k2l) {
-            addr_to_node[ADDR_TO_CACHE_LINE(k2l->data.address)] = k2l;
+            size_t cache_line = ADDR_TO_CACHE_LINE(k2l->data.address);
+            update_addr(cache_line, k2l);
         }
 
         return k2;  /* New root */
