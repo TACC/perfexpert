5e51df11878d40b08f2cadb55801aeebd58e85f3
Ankit Goyal <ankit3goyal@gmail.com>
2014-11-08 17:17:22 -0600

    adding set associativity conflicts for level 2 cache

--- tools/macpo/analyze/include/set_cache_conflict_analysis.h
+++ tools/macpo/analyze/include/set_cache_conflict_analysis.h
@@ -14,7 +14,7 @@
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE.
  * 
- * Authors: Leonardo Fialho and Ashay Rane
+ * Authors: Leonardo Fialho, Ashay Rane and Ankit Goyal
  *
  * $HEADER$
  */
@@ -26,9 +26,17 @@
 #include "avl_tree.h"
 #include "histogram.h"
 
+// This method iterates through the trace and based on reuse distance
+// (maintained using AVL tree), calculates the conflicts.
 int set_cache_conflict_analysis(const global_data_t& global_data);
 
-struct conflict_t{
+// this method returns the set number to which an address
+// maps to in a cache
+unsigned address_to_set(size_t address);
+
+// this struct is used to store a coflict when we are
+// 'almost' certain. There are no probabilities involved.
+struct conflict_t {
     size_t set_number;
     size_t var_idx;
 
@@ -36,6 +44,17 @@ struct conflict_t{
         set_number(_s), var_idx(_v) {}
 };
 
+// this struct is used to store a conflict probability
+struct conflict_prob_t {
+    size_t set_number;
+    size_t var_idx;
+    double miss_probability;
+
+    conflict_prob_t(size_t _s, size_t _v, double _mp) :
+        set_number(_s), var_idx(_v), miss_probability(_mp) {}
+};
+
 typedef std::vector<conflict_t> conflict_list_t;
+typedef std::vector<conflict_prob_t> conflict_prob_list_t;
 
 #endif /* SET_CACHE_CONFLICT_ANALYSIS_H_ */
--- tools/macpo/analyze/set_cache_conflict_analysis.cpp
+++ tools/macpo/analyze/set_cache_conflict_analysis.cpp
@@ -14,7 +14,7 @@
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE.
  * 
- * Authors: Leonardo Fialho and Ashay Rane
+ * Authors: Leonardo Fialho, Ashay Rane and Ankit Goyal
  *
  * $HEADER$
  */
@@ -28,6 +28,8 @@
 #include "histogram.h"
 
 #include "set_cache_conflict_analysis.h"
+#include "associative_cache.h"
+
 // TODO(goyalankit): remove hardcoded associativity
 #define ASSOCIATIVITY 8
 
@@ -68,15 +70,52 @@ void print_set_conflicts(std::vector<conflict_list_t> &conflicts,
     }
 }
 
+void print_set_conflicts_for_l2_cache(std::vector< conflict_prob_list_t >
+        &conflicts, int num_sets) {
+    
+    std::map< int, std::map<int, double>* > core_set_var_conf;
+    std::vector<int> per_set_conflict_num(num_sets, 0);
+    double total_miss_prob = 0.0;
+    int counter;
+    for (int core_id = 0; core_id < conflicts.size(); core_id++) {
+        core_set_var_conf[core_id] = new std::map<int, double>();
+        for (int n = 0; n < conflicts[core_id].size(); n++) {
+            conflict_prob_t ct = conflicts[core_id][n];
+            counter++;
+            total_miss_prob += ct.miss_probability;
+            per_set_conflict_num[ct.set_number] +=  1;
+            (*core_set_var_conf[core_id])[ct.set_number] += ct.miss_probability;
+        }
+    }
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "      L2 Cache Conflicts        " << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+    for (int core_id = 0; core_id < conflicts.size(); core_id++) {
+        if (core_set_var_conf[core_id]->size() != 0)
+            std::cout << "Conflicts for core number: " << core_id << std::endl;
+
+        for (std::map<int, double>::iterator it = core_set_var_conf[core_id]->begin();
+                it != core_set_var_conf[core_id]->end(); ++it) {
+            if (per_set_conflict_num[it->first] == 0) continue;
+            double conflict_prob = it->second / per_set_conflict_num[it->first];
+            std::cout << it->first << " " << conflict_prob  << std::endl;
+        }
+    }
+}
+
 int set_cache_conflict_analysis(const global_data_t& global_data) {
     const mem_info_bucket_t& bucket = global_data.mem_info_bucket;
     const int num_cores = sysconf(_SC_NPROCESSORS_CONF);
     const int num_streams = global_data.stream_list.size();
     cache_data_t l1_data = global_data.l1_data;
+    cache_data_t l2_data = global_data.l2_data;
+
     l1_data.associativity = ASSOCIATIVITY;
+    l2_data.associativity = ASSOCIATIVITY;
     const int num_sets = l1_data.size/
         (l1_data.line_size  * l1_data.associativity);
-    std::vector< conflict_list_t > set_conflicts_per_core(num_cores);
+    std::vector< conflict_list_t > set_conflicts_per_core_for_l1_cache(num_cores);
+    std::vector< conflict_prob_list_t > set_conflicts_per_core_for_l2_cache(num_cores);
 
     // list of per set avl trees for each core
     std::vector<avl_tree_list_t *> avl_trees_per_core(num_cores);
@@ -94,9 +133,10 @@ int set_cache_conflict_analysis(const global_data_t& global_data) {
         const mem_info_list_t& list = bucket.at(i);
 
         unsigned set_id = 0;
-        int reuse_distance;
+        long reuse_distance;
         size_t core_id;
         size_t var_idx, cache_line;
+        double l2_hit_probability;
         for (int j = 0; j < list.size(); j++) {
             const mem_info_t& mem_info = list.at(j);
             var_idx = mem_info.var_idx;
@@ -104,8 +144,10 @@ int set_cache_conflict_analysis(const global_data_t& global_data) {
             set_id = address_to_set(mem_info.address);
             core_id = mem_info.coreID;
             avl_tree_list_t &set_avl_list = (*avl_trees_per_core[core_id]);
-            std::vector<conflict_t> &set_conflicts =
-                set_conflicts_per_core[core_id];
+            conflict_list_t &set_conflicts_for_l1_cache =
+                set_conflicts_per_core_for_l1_cache[core_id];
+            conflict_prob_list_t &set_conflicts_for_l2_cache =
+                set_conflicts_per_core_for_l2_cache[core_id];
 
             // Quick validation check
             if (core_id >= 0 && core_id < num_cores ||
@@ -114,17 +156,26 @@ int set_cache_conflict_analysis(const global_data_t& global_data) {
                 if (set_avl_list[set_id]->contains(cache_line)) {
                     reuse_distance = set_avl_list[set_id]->
                         get_distance(cache_line);
+
+                    // L1 conflicts
                     if (reuse_distance > l1_data.associativity) {
                         conflict_t conflict(set_id, var_idx);
-                        set_conflicts.push_back(conflict);
+                        set_conflicts_for_l1_cache.push_back(conflict);
                     }
+
+                    // L2 probabilistic conflicts
+                    l2_hit_probability = hit_probability(reuse_distance, l2_data.size,
+                            l2_data.associativity, l2_data.line_size);
+                    conflict_prob_t conflict_prob(set_id, var_idx, (1 - l2_hit_probability));
+                    set_conflicts_for_l2_cache.push_back(conflict_prob);
                 }
                 set_avl_list[set_id]->insert(&mem_info);
             }
         }
     }
 
-    print_set_conflicts(set_conflicts_per_core, num_sets);
+    print_set_conflicts(set_conflicts_per_core_for_l1_cache, num_sets);
+    print_set_conflicts_for_l2_cache(set_conflicts_per_core_for_l2_cache, num_sets);
 
     return 0;
 }
