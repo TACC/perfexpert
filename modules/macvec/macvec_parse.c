855ef04b9566e23a9ef31e231feb56a034ac7819
Leonardo Fialho <leonardofialho@gmail.com>
2014-11-14 14:34:41 -0600

    Still testing...

--- modules/macvec/macvec_parse.c
+++ modules/macvec/macvec_parse.c
@@ -62,47 +62,50 @@ const char* msg_branch_eval = "Indicate branch evaluations";
 const char* msg_float_conv = "Use limited types";
 
 const char* dsc_ptr_check = "If the compiler is unaware that pointers do not "
-        "overlap in memory, the compiler's dependence analysis may infer "
-        "existence of vector dependence. If the pointers used in this loop "
-        "body indeed do not overlap, declare them with the 'restrict' keyword. "
-        "For instance: double* restrict ptr_a;. See "
-        "https://software.intel.com/en-us/articles/vectorization-with-the-intel-compilers-part-i for additional details.";
+    "overlap in memory, the compiler's dependence analysis may infer "
+    "existence of vector dependence. If the pointers used in this loop "
+    "body indeed do not overlap, declare them with the 'restrict' keyword. "
+    "For instance: double* restrict ptr_a;. See "
+    "https://software.intel.com/en-us/articles/vectorization-with-the-intel"
+    "-compilers-part-i for additional details.";
 
 const char* dsc_trip_count = "The compiler's vectorization cost model can be "
-        "takes various factors into account, with the loop trip count being "
-        "one among them. Informing the compiler about loop trip counts that "
-        "cannot be statically inferred helps the compiler make better "
-        "decisions about the vectorizability of the code. If loop trip count "
-        "is known, add \"#pragma loop_count(n)\" directive before the loop, "
-        "where 'n' is equal to the loop count. See "
-        "https://software.intel.com/en-us/node/524502 for additional details.";
+    "takes various factors into account, with the loop trip count being "
+    "one among them. Informing the compiler about loop trip counts that "
+    "cannot be statically inferred helps the compiler make better "
+    "decisions about the vectorizability of the code. If loop trip count "
+    "is known, add \"#pragma loop_count(n)\" directive before the loop, "
+    "where 'n' is equal to the loop count. See "
+    "https://software.intel.com/en-us/node/524502 for additional details.";
 
 const char* dsc_mem_align = "Aligned memory accesses usually incur much lower "
-        "penalty of access (when compared with unaligned acccesses), "
-        "especially on the Intel Xeon Phi coprocessor. To align arrays and "
-        "structures, use __attribute__((aligned(64))) for statically-allocated "
-        "memory and _mm_malloc() for dynamically allocated memory. See "
-        "https://software.intel.com/en-us/articles/data-alignment-to-assist-vectorization "
-        "for additional details.";
+    "penalty of access (when compared with unaligned acccesses), "
+    "especially on the Intel Xeon Phi coprocessor. To align arrays and "
+    "structures, use __attribute__((aligned(64))) for statically-allocated "
+    "memory and _mm_malloc() for dynamically allocated memory. See "
+    "https://software.intel.com/en-us/articles/data-alignment-to-assist-"
+    "vectorization for additional details.";
 
 const char* dsc_branch_eval = "Branches that evaluate to always true or always "
-        "false can be indicated to the compiler to change code layout for "
-        "optimization. Use the \"__builtin_expect()\" intrinsic function to "
-        "indicate branch outcomes that are highly biased.";
+    "false can be indicated to the compiler to change code layout for "
+    "optimization. Use the \"__builtin_expect()\" intrinsic function to "
+    "indicate branch outcomes that are highly biased.";
 
 const char* dsc_float_conv = "Using mixed precision (single- as well as double-"
-        "precision) values in a loop body introduces many inefficiences in "
-        "vectorized code generation. The inefficiencies arise in choosing "
-        "the appropriate alignment, converting intermediate results from one "
-        "format to another and in estimating the cost of vectorization. If "
-        "possible, convert all floating point values to use the same "
-        "precision.";
+    "precision) values in a loop body introduces many inefficiences in "
+    "vectorized code generation. The inefficiencies arise in choosing "
+    "the appropriate alignment, converting intermediate results from one "
+    "format to another and in estimating the cost of vectorization. If "
+    "possible, convert all floating point values to use the same "
+    "precision.";
 
 static regex_t re_dep_var;
 
 void extract_var(const char* line, location_t* location) {
     regmatch_t pmatch[5] = {0};
     if (regexec(&re_dep_var, line, 5, pmatch, 0) == 0) {
+        var_t *var_01, *var_02;
+
         int start = pmatch[1].rm_so;
         int length = pmatch[1].rm_eo - pmatch[1].rm_so;
 
@@ -126,9 +129,12 @@ void extract_var(const char* line, location_t* location) {
         long line_02 = strtol(line + pmatch[2].rm_so, NULL, 10);
 
         // Add name_01, line_01 and name_02, line_02 to location_t::var_list.
-        var_t *var_01, *var_02;
+
         PERFEXPERT_ALLOC(var_t, var_01, sizeof(var_t));
         PERFEXPERT_ALLOC(var_t, var_02, sizeof(var_t));
+
+        perfexpert_list_item_construct((perfexpert_list_item_t *) var_01);
+        perfexpert_list_item_construct((perfexpert_list_item_t *) var_02);
 
         snprintf(var_01->name, k_var_len, "%s", name_01);
         snprintf(var_02->name, k_var_len, "%s", name_02);
@@ -136,33 +142,26 @@ void extract_var(const char* line, location_t* location) {
         var_01->line_number = line_01;
         var_02->line_number = line_02;
 
-        perfexpert_list_item_t* list_item = NULL;
-        list_item = (perfexpert_list_item_t*) var_01;
-        perfexpert_list_item_construct(list_item);
-        perfexpert_list_append(&(location->var_list), list_item);
-
-        // TODO: Will uncomment after infinite-loop problem is resolved.
-        // list_item = (perfexpert_list_item_t*) var_02;
-        // perfexpert_list_item_construct(list_item);
-        // perfexpert_list_append(&(location->var_list), list_item);
+        perfexpert_list_append(&(location->var_list), (perfexpert_list_item_t *)var_01);
+        perfexpert_list_append(&(location->var_list), (perfexpert_list_item_t *)var_02);
     }
 }
 
 const message_t messages[] = {
-    {   15046,  &msg_ptr_check,     &dsc_ptr_check,     NULL,           },
-    {   15344,  &msg_ptr_check,     &dsc_ptr_check,     NULL,           },
-    {   15346,  &msg_ptr_check,     NULL,               &extract_var,   },
-    {   15037,  &msg_trip_count,    &dsc_trip_count,    NULL,           },
-    {   15017,  &msg_trip_count,    &dsc_trip_count,    NULL,           },
-    {   15315,  &msg_trip_count,    &dsc_trip_count,    NULL,           },
-    {   15523,  &msg_trip_count,    &dsc_trip_count,    NULL,           },
-    {   15167,  &msg_mem_align,     &dsc_mem_align,     NULL,           },
-    {   15524,  &msg_mem_align,     &dsc_mem_align,     NULL,           },
-    {   15421,  &msg_mem_align,     &dsc_mem_align,     NULL,           },
-    {   15167,  &msg_mem_align,     &dsc_mem_align,     NULL,           },
-    {   15038,  &msg_branch_eval,   &dsc_branch_eval,   NULL,           },
-    {   15336,  &msg_branch_eval,   &dsc_branch_eval,   NULL,           },
-    {   15327,  &msg_float_conv,    &dsc_float_conv,    NULL,           },
+    { 15046, &msg_ptr_check,   &dsc_ptr_check,   NULL,         },
+    { 15344, &msg_ptr_check,   &dsc_ptr_check,   NULL,         },
+    { 15346, &msg_ptr_check,   NULL,             &extract_var, },
+    { 15037, &msg_trip_count,  &dsc_trip_count,  NULL,         },
+    { 15017, &msg_trip_count,  &dsc_trip_count,  NULL,         },
+    { 15315, &msg_trip_count,  &dsc_trip_count,  NULL,         },
+    { 15523, &msg_trip_count,  &dsc_trip_count,  NULL,         },
+    { 15167, &msg_mem_align,   &dsc_mem_align,   NULL,         },
+    { 15524, &msg_mem_align,   &dsc_mem_align,   NULL,         },
+    { 15421, &msg_mem_align,   &dsc_mem_align,   NULL,         },
+    { 15167, &msg_mem_align,   &dsc_mem_align,   NULL,         },
+    { 15038, &msg_branch_eval, &dsc_branch_eval, NULL,         },
+    { 15336, &msg_branch_eval, &dsc_branch_eval, NULL,         },
+    { 15327, &msg_float_conv,  &dsc_float_conv,  NULL,         },
 };
 
 const long message_count = sizeof(messages) / sizeof(message_t);
@@ -185,15 +184,17 @@ macvec_hotspot_t* find(perfexpert_list_t* hotspots, char* filename, int line) {
 }
 
 int process_file(perfexpert_list_t* hotspots, FILE* stream,
-        regex_t* re_loop, regex_t* re_remark, perfexpert_list_t* locations) {
+    regex_t* re_loop, regex_t* re_remark, perfexpert_list_t* locations) {
     char file_line[k_line_len];
 
     perfexpert_list_t stack;
     perfexpert_list_construct(&stack);
 
+    OUTPUT(("%s [%d]", _YELLOW("TEST A"), perfexpert_list_get_size(locations)));
+
     const char* dep_var_pattern = "remark #15346: vector dependence: assumed "
-            "[A-Z]* dependence between ([^ ]*) line ([0-9]*) and ([^ ]*) line "
-            "([0-9]*)";
+        "[A-Z]* dependence between ([^ ]*) line ([0-9]*) and ([^ ]*) line "
+        "([0-9]*)";
     if (regcomp(&re_dep_var, dep_var_pattern, REG_EXTENDED) != 0) {
         return -ERR_REGEX;
     }
@@ -201,22 +202,25 @@ int process_file(perfexpert_list_t* hotspots, FILE* stream,
     while (fgets(file_line, k_line_len, stream)) {
         regmatch_t pmatch[3] = {0};
         if (strstr(file_line, "LOOP END") != NULL) {
-            if (perfexpert_list_get_size(&stack) != 0) {
-                perfexpert_list_item_t* last = NULL;
-                last = perfexpert_list_get_last(&stack);
-                location_t* location = (location_t*) last;
+            if (0 != perfexpert_list_get_size(&stack)) {
+                location_t *location = (location_t*)perfexpert_list_get_last(&stack);
                 if (location->ignored == 0) {
                     /* Copy this location into `locations'. */
                     location_t* loc;
                     PERFEXPERT_ALLOC(location_t, loc, sizeof(location_t));
                     memcpy(loc, location, sizeof(location_t));
-                    perfexpert_list_item_t* list_item = NULL;
-                    list_item = (perfexpert_list_item_t*) loc;
-                    perfexpert_list_item_construct(list_item);
-                    perfexpert_list_append(locations, list_item);
+                    perfexpert_list_construct(&(loc->var_list));
+                    perfexpert_list_item_construct((perfexpert_list_item_t *)loc);
+                    perfexpert_list_append(locations, (perfexpert_list_item_t *)loc);
+
+                    OUTPUT(("%s [%d]", _YELLOW("TEST B"),
+                        perfexpert_list_get_size(locations)));
+
+                    OUTPUT_VERBOSE((2, "Adding new hotspot: %s:%d",
+                        location->function, location->line_number));
                 }
 
-                perfexpert_list_remove_item(&stack, last);
+                perfexpert_list_remove_item(&stack, (perfexpert_list_item_t *)location);
             }
         } else if (regexec(re_loop, file_line, 3, pmatch, 0) == 0) {
             int start = pmatch[1].rm_so;
@@ -238,6 +242,8 @@ int process_file(perfexpert_list_t* hotspots, FILE* stream,
             loc->remark_count = 0;
             loc->line_number = line_number;
             perfexpert_list_construct(&(loc->var_list));
+            perfexpert_list_item_construct((perfexpert_list_item_t *)loc);
+            perfexpert_list_append(&stack, (perfexpert_list_item_t *)loc);
 
             if (hotspot != NULL) {
                 loc->ignored = 0;
@@ -249,17 +255,17 @@ int process_file(perfexpert_list_t* hotspots, FILE* stream,
                 loc->function[0] = '\0';
             }
 
-            perfexpert_list_item_t* list_item = (perfexpert_list_item_t*) loc;
-            perfexpert_list_item_construct(list_item);
-            perfexpert_list_append(&stack, list_item);
-        } else if (perfexpert_list_get_size(&stack) != 0) {
-            perfexpert_list_item_t* last = NULL;
-            last = perfexpert_list_get_last(&stack);
-            location_t* location = (location_t*) last;
-            if (location->ignored == 0 && regexec(re_remark, file_line, 2,
-                        pmatch, 0) == 0) {
+            OUTPUT_VERBOSE((2, "Found loop at: %s:%d", loc->filename,
+                loc->line_number));
+
+        } else if (0 != perfexpert_list_get_size(&stack)) {
+            location_t *location = NULL;
+            location = (location_t *)perfexpert_list_get_last(&stack);
+
+            if (location->ignored == 0 &&
+                regexec(re_remark, file_line, 2, pmatch, 0) == 0) {
                 long remark_number = strtol(file_line + pmatch[1].rm_so, NULL,
-                        10);
+                    10);
 
                 /* Check if this is an "interesting" remark. */
                 uint32_t i = 0;
@@ -301,6 +307,8 @@ int process_file(perfexpert_list_t* hotspots, FILE* stream,
 
     regfree(&re_dep_var);
 
+    OUTPUT(("%s [%d]", _YELLOW("TEST C"), perfexpert_list_get_size(locations)));
+
     while (perfexpert_list_get_size(&stack) != 0) {
         perfexpert_list_item_t* last = NULL;
         last = perfexpert_list_get_last(&stack);
@@ -309,11 +317,10 @@ int process_file(perfexpert_list_t* hotspots, FILE* stream,
     }
 
     perfexpert_list_destruct(&stack);
-    return 0;
+    return SUCCESS;
 }
 
-int parse(perfexpert_list_t* hotspots, const char* report_filename,
-        perfexpert_list_t* locations) {
+int parse(perfexpert_list_t* hotspots, const char* report_filename, perfexpert_list_t* locations) {
     regex_t re_loop;
     const char* loop_pattern = "LOOP BEGIN at ([^\\(]*)\\(([0-9]*),([0-9]*)\\)";
     if (regcomp(&re_loop, loop_pattern, REG_EXTENDED) != 0) {
@@ -344,6 +351,8 @@ int parse(perfexpert_list_t* hotspots, const char* report_filename,
         return -ERR_FILE_CONTENTS;
     }
 
+    OUTPUT(("%s [%d]", _YELLOW("TEST D"), perfexpert_list_get_size(locations)));
+
     regfree(&re_remark);
     regfree(&re_loop);
     fclose(stream);
@@ -351,18 +360,22 @@ int parse(perfexpert_list_t* hotspots, const char* report_filename,
     return SUCCESS;
 }
 
-void print_recommendations(perfexpert_list_t* locations) {
+static void print_recommendations(perfexpert_list_t* locations) {
     location_t* location;
-    perfexpert_list_for (location, locations, location_t) {
-        fprintf(stdout, "\nLoop in function %s in %s:%d (%.2f%% of the total "
-                "runtime)\n", location->function, location->filename,
-                location->line_number, location->importance * 100);
 
-        if (perfexpert_list_get_size(&location->var_list) != 0) {
+    OUTPUT(("%s [%d]", _YELLOW("Printing recommendations"),
+        perfexpert_list_get_size(locations)));
+
+    perfexpert_list_for(location, locations, location_t) {
+        fprintf(stdout, "\nLoop in function %s in %s:%d (%.2f%% of the total "
+            "runtime)\n", location->function, location->filename,
+            location->line_number, location->importance * 100);
+
+        if (0 != perfexpert_list_get_size(&(location->var_list))) {
             var_t* var;
             fprintf(stdout, "Affected variables: [%d]: ",
-                    perfexpert_list_get_size(&location->var_list));
-            perfexpert_list_for (var, &(location->var_list), var_t) {
+                perfexpert_list_get_size(&(location->var_list)));
+            perfexpert_list_for(var, &(location->var_list), var_t) {
                 fprintf(stdout, "%s at line %ld  ", var->name,
                 var->line_number);
             }
@@ -391,15 +404,19 @@ void print_recommendations(perfexpert_list_t* locations) {
 int process_hotspots(perfexpert_list_t* hotspots, const char* report_filename) {
     perfexpert_list_t locations;
     perfexpert_list_construct(&locations);
+    int err = SUCCESS;
 
-    int err = parse(hotspots, report_filename, &locations);
-    if (err != SUCCESS) {
+    OUTPUT(("hello! [1]"));
+    if (SUCCESS != (err = parse(hotspots, report_filename, &locations))) {
         OUTPUT(("%s: %d",
-                _ERROR("Failed to parse vectorization report, err code"), err));
+            _ERROR("Failed to parse vectorization report, err code"), err));
         return PERFEXPERT_ERROR;
     }
-
+    OUTPUT(("hello! [2]"));
+    OUTPUT(("%s [%d]", _YELLOW("TEST F"), perfexpert_list_get_size(&locations)));
     print_recommendations(&locations);
+
+    OUTPUT(("hello! [3]"));
 
     while (perfexpert_list_get_size(&locations) != 0) {
         perfexpert_list_item_t* last = NULL;
