04f1bcf01ba8bf19ed76fec9b7b6dbac7081a41a
Leonardo Fialho <fialho@MacBook-Pro-retina.local>
2014-12-04 15:39:58 -0600

    Updating library usage example

--- lib/cache_sim/example/cache_frontend.c
+++ lib/cache_sim/example/cache_frontend.c
@@ -21,12 +21,13 @@
 
 /* headers */
 #include <stdio.h>
+#include <stdint.h>
 #include "cache_sim.h"
 
 /* main */
 int main(int argc, char *argv[]) {
     cache_handle_t *cache;
-    int i = 0;
+    uint64_t i = 0;
 
     if (NULL == (cache = cache_sim_init(32768, 64, 8, "lru"))) {
         printf("Error\n");
@@ -34,16 +35,21 @@ int main(int argc, char *argv[]) {
     } else {
         printf("Ok!\n");
     }
+    if (CACHE_SIM_SUCCESS != cache_sim_reuse_enable(cache, 513)) {
+        printf("Error\n");
+    } else {
+        printf("Ok!\n");
+    }
 
-    for (i = 0; i < 4096; i+=4) {
-        cache->access_fn(cache, i);
+    for (i = 0; i < 1024*32768; i+=32) {
+        cache_sim_access(cache, i);
     }
-    for (i = 0; i < 1024*8*4096; i+=4) {
-        cache->access_fn(cache, i);
-    }
-    for (i = 0; i < 4096; i+=4) {
-        cache->access_fn(cache, i);
-    }
+    // for (i = 0; i < 2*2*2*4*2*2*8*8*8*4096; i+=4) {
+    //     cache_sim_access(cache, i);
+    // }
+    // for (i = 0; i < 32768; i+=4) {
+    //     cache_sim_access(cache, i);
+    // }
 
     cache_sim_fini(cache);
 
