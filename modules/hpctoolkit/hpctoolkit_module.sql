3b10bed1556089e9556d09acb9b3f24da1a71774
antoniogi <antoniogi@gmail.com>
2016-01-28 09:33:21 -0600

    Cleanup

--- modules/hpctoolkit/hpctoolkit_module.sql
+++ modules/hpctoolkit/hpctoolkit_module.sql
@@ -1,5 +1,5 @@
 --
--- Copyright (c) 2011-2015  University of Texas at Austin. All rights reserved.
+-- Copyright (c) 2011-2016  University of Texas at Austin. All rights reserved.
 --
 -- $COPYRIGHT$
 --
@@ -14,7 +14,7 @@
 -- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 -- A PARTICULAR PURPOSE.
 --
--- Authors: Leonardo Fialho and Ashay Rane
+-- Authors: Antonio Gomez-Iglesias, Leonardo Fialho and Ashay Rane
 --
 -- $HEADER$
 --
@@ -23,29 +23,3 @@
 -- Enable foreign keys
 --
 PRAGMA foreign_keys = ON;
-
---
--- Create tables if not exist
---
---CREATE TABLE IF NOT EXISTS hpctoolkit_hotspot (
---    perfexpert_id INTEGER NOT NULL,
---    id            INTEGER PRIMARY KEY,
---    name          VARCHAR NOT NULL,
---    type          INTEGER NOT NULL,
---    profile       VARCHAR NOT NULL,
---    module        VARCHAR NOT NULL,
---    file          VARCHAR NOT NULL,
---    line          INTEGER NOT NULL,
---    depth         INTEGER NOT NULL,
---    relevance     INTEGER
---);
-
---CREATE TABLE IF NOT EXISTS hpctoolkit_event (
---    id            INTEGER PRIMARY KEY,
---    name          VARCHAR NOT NULL,
---    thread_id     INTEGER NOT NULL,
---    mpi_task      INTEGER NOT NULL,
---    experiment    INTEGER NOT NULL,
---    value         REAL    NOT NULL,
---    hotspot_id    INTEGER REFERENCES hotspot
---);
