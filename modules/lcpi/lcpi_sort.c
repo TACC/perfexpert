0479d3732cafbfadaa7affc9eef23ad913d2f70b
antoniogi <antoniogi@gmail.com>
2016-05-05 10:00:52 -0500

    Updated copyright

--- modules/lcpi/lcpi_sort.c
+++ modules/lcpi/lcpi_sort.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013  University of Texas at Austin. All rights reserved.
+ * Copyright (c) 2011-2016  University of Texas at Austin. All rights reserved.
  *
  * $COPYRIGHT$
  *
@@ -14,7 +14,7 @@
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE.
  *
- * Authors: Leonardo Fialho and Ashay Rane
+ * Authors: Antonio Gomez-Iglesias, Leonardo Fialho and Ashay Rane
  *
  * $HEADER$
  */
