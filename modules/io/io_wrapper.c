f558440a8ebcb5f134f545870669eabcad4d4d6e
antoniogi <antoniogi@gmail.com>
2017-03-29 15:14:27 -0500

    Cleaning

--- modules/io/io_wrapper.c
+++ modules/io/io_wrapper.c
@@ -63,112 +63,7 @@ static FILE* (*real_fopen)(const char *path, const char *mode) = NULL;
 static int (*real_close)(int fd);
 static int (*real_fclose)(FILE *stream) = NULL;
 
-my_module_globals_t my_module_globals; //OJO
-
-int perfexpert_unwind_get_file_line (unw_word_t addr, char *file, size_t len, int *line, char *executable) {
-    char buf[256];
-    char *p; 
-
-    if (executable == NULL)
-        return -1;
-    // prepare command to be executed
-    // our program need to be passed after the -e parameter
-//    snprintf (buf, 256, "/usr/bin/addr2line -C -e %s -f -i %lx", executable, addr);
-    //snprintf (buf, 256, "/usr/bin/ls");
-    //sprintf (buf, "/usr/bin/addr2line -C -e ./example -f -i %lx", addr);
- //   printf ("Running this command: %s\n", buf);
-    //return 1;
-
-    //FILE* f = popen (buf, "r");
-    pid_t pid;
-    fflush(stdout);
-    switch(forkpty(&pid, NULL, NULL, NULL)) {
-        case -1:
-            perror ("Error while forkpty");
-            exit (-1);
-        case 0:
-  //          execl ("/usr/bin/addr2line", "addr2line", "-C", "-e", executable, "-f", "-i" , addr, NULL);
-            execl ("/usr/bin/ls", "ls", NULL);
-            exit(0);
-        default: {
-  //          while (1) {
-                int ret;
-                char out[256] = {0};
-                if ((ret = read(pid, out, 255)) < 0) {
-           //         perror ("Reading from child process");
-                    break;
-                    //exit(-1);
-                }
-                out[ret]='\0';
-                printf ("Buffer1: %s\n", out);
-                if ((ret = read(pid, out, 255)) < 0) {
-           //         perror ("Reading from child process");
-                    break;
-                    //exit(-1);
-                }
-                out[ret]='\0';
-                printf ("Buffer2: %s\n", out);
-            }
-//                 }
-    }
-    printf ("pid is %d\n", pid);
-    kill (pid, SIGKILL);
-    printf ("Bye\n");
-//    int flags;
-//    int fd = fileno(f);
-//    flags = fcntl (fd, F_GETFL, 0);
-//    flags |= O_NONBLOCK;
-//    fcntl (fd, F_SETFL, flags);
-
-    /*  
-    if (f == NULL)
-    {  
-        printf ("f is NULL!! \n"); 
-        perror (out);
-        return 0;
-    }   
-
-    printf ("f was correct\n");
-    while (fgets(out, 256, f) != NULL) {
-        printf ("Buffer: %s\n", out);
-    }
-    printf ("Out of the loop\n");
-//    printf ("Bye\n");
-    // get function name
-   // fgets (out, 256, f); 
-
-   // printf ("Buffer: %s\n", out);
-    pclose(f);
-    printf ("Bye\n");
-    return 0;
-    // get file and line
-    fgets (out, 256, f); 
-    printf ("Buffer: %s\n", out);
-    printf ("Length: %d\n", strlen(out));
-
-    if (out[0] != '?') {
-        int l;
-        int i = 0;
-        char *p = out;
-
-        // file name is until ':'
-        while (*p != ':') {   
-            p++;
-        }
-
-        *p++ = 0;
-        // after file name follows line number
-        strcpy (file , out);
-        sscanf (p,"%d", line);
-    }
-    else {
-        strcpy (file,"unkown");
-        *line = 0;
-    }
-    */
-//    pclose(f);
-    return 1;
-}
+my_module_globals_t my_module_globals; 
 
 void add_function_to_data(char * function_name, long address, int function) {
     int found = 0;
@@ -224,28 +119,18 @@ void capture_backtrace(char *executable, int function) {
             
             printf ("Adding function %s and address %lu\n", name, (long) ip);
             add_function_to_data (name, (long)ip, function);
-            //perfexpert_unwind_get_file_line ((long)ip, file, 256, &line, executable);
 
             level++;
         }
-
-/*  
-        //if (line==0)
-        //    break;
-        //if (strlen(file)<2)
-        //    break;
-
-        if (function ==FOPEN)
-            printf("[fopen] %s in file %s line %d\n", name, file, line);
-        if (function ==FREAD)
-            printf("[fread] %s in file %s line %d\n", name, file, line);
-        if (function ==FWRITE)
-            printf("[fwrite] %s in file %s line %d\n", name, file, line);
-*/
     }
     printf ("DONE WITH BACKTRACE\n");
 }
 
+void finish() __attribute__ ((destructor));
+void finish() {
+//Write my_module_globals.data to file
+//
+}
 /*
 void init() __attribute__ ((constructor));
 void init() {
@@ -261,32 +146,12 @@ void init() {
 }
 */
 
-/*  int open(const char *path, int oflag, ... ) { 
-    va_list argp;
-    va_start(argp, oflag);
-    real_open=dlsym(RTLD_NEXT, "open");
-    int ret = real_open=(path, oflag, argp);
-    va_end(argp);
-    return ret;
-}
-*/
-
 FILE* fopen(const char *path, const char *mode) {
     program = getenv("PERFEXPERT_PROGRAM");
     capture_backtrace (program, FOPEN);
     real_fopen=dlsym(RTLD_NEXT, "fopen");
     return real_fopen(path, mode);
 }
-
-
-
-/*  ssize_t write(int fd, const void *buf, size_t count) {
-    printf ("Writing %lu\n", count);
-    capture_backtrace(program);
-    real_write=dlsym(RTLD_NEXT, "write");
-    return real_write(fd,buf,count);
-}
-*/
 
 size_t fwrite(const void * ptr, size_t size, size_t n, FILE * s) {
     program = getenv("PERFEXPERT_PROGRAM");
@@ -295,28 +160,12 @@ size_t fwrite(const void * ptr, size_t size, size_t n, FILE * s) {
     return real_fwrite(ptr, size, n, s);
 }
 
-/*  ssize_t read(int fd, void *buf, size_t count) {
-    printf ("Reading %lu\n", count);
-    capture_backtrace (program);
-    real_read=dlsym(RTLD_NEXT, "read");
-    return real_read(fd,buf,count);
-}
-*/
-
 size_t fread(void *ptr, size_t size, size_t n, FILE * s) {
     program = getenv("PERFEXPERT_PROGRAM");
     capture_backtrace (program, FREAD);
     real_fread=dlsym(RTLD_NEXT, "fread");
     return real_fread(ptr, size, n, s);
 }
-
-/*  int close(int fd) {
-    printf ("Closing\n");
-    capture_backtrace (program);
-    real_close=dlsym(RTLD_NEXT, "close");
-    return real_close(fd);
-}
-*/
 
 int fclose(FILE *stream) {
     program = getenv("PERFEXPERT_PROGRAM");
